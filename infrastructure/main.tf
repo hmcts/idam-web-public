c04289bc8c8f8c1bb443a07051185cf850f5105f
Kremena Nenkova <kremena.nenkova@hmcts.net>
2018-05-10 22:40:32 +0100

    SIDM-1143: Infra changes

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -6,10 +6,9 @@ module "idam-web-public" {
   ilbIp                 = "${var.ilbIp}"
   is_frontend           = false
   subscription          = "sandbox"
-  additional_host_name  = "${var.external_host_name}"
   capacity     = "${var.capacity}"
 
   app_settings = {
-    STRATEGIC_SERVICE_URL = "http://idam-api-${var.env}.service.${data.terraform_remote_state.core_apps_compute.ase_name[0]}.internal"
+
   }
 }
