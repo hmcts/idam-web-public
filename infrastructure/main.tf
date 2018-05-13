7634f7aea3d83205d6c878fb1da8b85ebd29c415
Paul Verity <paul@verityit.net>
2018-05-13 04:52:11 +0100

    SIDM-1143: set is_frontend to true
    Solo: @verityit

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -2,10 +2,10 @@ module "idam-web-public" {
   source                = "git@github.com:hmcts/moj-module-webapp?ref=master"
   product               = "${var.product}-${var.app}"
   location              = "${var.location}"
-  env                   = "idam-sandbox"
+  env                   = "${var.env}"
   ilbIp                 = "${var.ilbIp}"
-  is_frontend           = false
-  subscription          = "sandbox"
+  is_frontend           = true
+  subscription          = "${var.subscription}"
   capacity              = "${var.capacity}"
   additional_host_name  = "${var.external_host_name}"
 
--- infrastructure/state.tf
+++ infrastructure/state.tf
@@ -6,7 +6,7 @@ data "terraform_remote_state" "core-infra" {
   backend = "azurerm"
 
   config {
-    resource_group_name  = "mgmt-state-store-${replace(var.env,"idam-","")}"
+    resource_group_name  = "mgmt-state-store-${var.subscription}"
     storage_account_name = "mgmtstatestore${var.subscription}"
     container_name       = "mgmtstatestorecontainer${var.env}"
     key                  = "core-infra/${var.env}/terraform.tfstate"
