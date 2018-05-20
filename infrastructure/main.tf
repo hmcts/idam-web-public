1c7a771fc480c287c966f86354215e2f1b170de6
Kremena Nenkova <kremena.nenkova@hmcts.net>
2018-05-20 22:02:02 +0100

    Enable SSL verification for idam-prod.

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -14,8 +14,11 @@ module "idam-web-public" {
   additional_host_name  = "hmcts-access.${replace(var.env, "idam-", "")}.platform.hmcts.net"
 
   app_settings = {
-    MANAGEMENT_SECURITY_ENABLED = "${var.env == "idam-prod" ? "true" : "false"}"
+    MANAGEMENT_SECURITY_ENABLED   = "${var.env == "idam-prod" ? "true" : "false"}"
 
-    STRATEGIC_SERVICE_URL = "https://idam-api-${local.integration_env}.service.core-compute-${local.integration_env}.internal"
+    // remove when SSL certificates are in place
+    SSL_VERIFICATION_ENABLED      = "${var.env == "idam-prod" ? "true" : "false"}"
+
+    STRATEGIC_SERVICE_URL         = "https://idam-api-${local.integration_env}.service.core-compute-${local.integration_env}.internal"
   }
 }
