285ef5e4830a1ac7a7ca97d7b021e89dadf3a2f1
Kremena Nenkova <kremena.nenkova@hmcts.net>
2018-05-22 22:06:06 +0100

    Added integration test for Spring Boot context.

---
+++ src/test/java/uk/gov/hmcts/reform/idam/web/ApplicationIntegrationTest.java
@@ -0,0 +1,16 @@
+package uk.gov.hmcts.reform.idam.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(properties = {"ssl.verification.enabled=false"})
+public class ApplicationIntegrationTest {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
