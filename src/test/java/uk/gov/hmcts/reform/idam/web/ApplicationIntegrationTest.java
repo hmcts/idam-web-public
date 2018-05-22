0c13b0b4c69d215b45fdc327b1ae917f60b8fbdd
Kremena Nenkova <kremena.nenkova@hmcts.net>
2018-05-22 22:11:27 +0100

    Added integration test for Spring Boot context.

--- src/test/java/uk/gov/hmcts/reform/idam/web/ApplicationIntegrationTest.java
+++ src/test/java/uk/gov/hmcts/reform/idam/web/ApplicationIntegrationTest.java
@@ -10,7 +10,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class ApplicationIntegrationTest {
 
     @Test
-    public void contextLoads() {
+    public void applicationContext_shouldLoad() {
     }
 
 }
