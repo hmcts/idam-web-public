13a534b71933e28032a55148439ebb23a9b2f9d3
Kremena Nenkova <kremena.nenkova@hmcts.net>
2018-06-12 11:47:40 +0100

    SIDM-1445: using agreed additional hostname for Web Public.

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -7,7 +7,7 @@ module "idam-web-public" {
   is_frontend           = "${var.env == "idam-preview" ? 0 : 1}"
   subscription          = "${var.subscription}"
   capacity              = "${var.capacity}"
-  additional_host_name  = "hmcts-access.${replace(var.env, "idam-", "")}.platform.hmcts.net"
+  additional_host_name  = "idam-web-public.${replace(var.env, "idam-", "")}.platform.hmcts.net"
   appinsights_instrumentation_key = "${var.appinsights_instrumentation_key}"
 
   app_settings = {
