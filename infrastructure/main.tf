57c7169e5311f739d865e4f1c022e96c8ec645df
Steve Clewer <steve.clewer@amido.com>
2018-07-24 10:50:50 +0100

    SIDM-762: More testing

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -32,6 +32,6 @@ module "idam-web-public" {
 
     STRATEGIC_SERVICE_URL         = "${local.idam_api}"
 
-    GA_TRACKER_ID                 = "UA-DUMMY-X"
+    GA_TRACKER_ID                 = "${var.env == "idam-preview" ? "UA-DUMMY-XXX" : "UA-DUMMY-YYY"}"
   }
 }
