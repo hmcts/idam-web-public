6d17407f08c8700625e9352d5a17c94ca9b98e37
jburke-amido <james.burke@amido.com>
2018-08-06 14:23:00 +0100

    trying to fix errors

--- pa11y.conf.js
+++ pa11y.conf.js
@@ -1,6 +1,9 @@
 const TestData = require('./src/test/js/config/test_data')
 
 module.exports = {
-    timeout: 60000,
-    allowedStandards: ['WCAG2AA', 'HMCTS Standards']
+    timeout: 360000,
+    allowedStandards: ['WCAG2AA', 'HMCTS Standards'],
+    chromeLaunchConfig: {
+      ignoreHTTPSErrors: false
+    }
 };
