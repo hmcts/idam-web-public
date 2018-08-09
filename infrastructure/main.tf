93afaf173979d95321db26f221fb16bf6cfe0a60
kremi <34029797+kremi@users.noreply.github.com>
2018-08-09 11:37:49 +0100

    SIDM-1165: Prod URLs (and using HTTP in SAAT) (#64)

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -6,6 +6,9 @@ locals {
   vault_uri = "https://${local.vault_name}.vault.azure.net/"
   secure_actuator_endpoints = "${var.env == "idam-prod" || var.env == "idam-demo" ? true : false}"
 
+  default_external_host_name = "idam-web-public.${replace(var.env, "idam-", "")}.platform.hmcts.net"
+  external_host_name = "${var.external_host_name_override != "" ? var.external_host_name_override : local.default_external_host_name}"
+
   default_idam_api = "https://idam-api.${replace(var.env, "idam-", "")}.platform.hmcts.net"
   idam_api_url = "${var.idam_api_url_override != "" ? var.idam_api_url_override : local.default_idam_api}"
   idam_api_testing_support_url = "${var.idam_api_testing_support_url_override != "" ? var.idam_api_testing_support_url_override : local.idam_api_url}"
@@ -22,7 +25,7 @@ module "idam-web-public" {
   subscription          = "${var.subscription}"
   capacity              = "${var.capacity}"
   https_only            = "${var.https_only}"
-  additional_host_name  = "idam-web-public.${replace(var.env, "idam-", "")}.platform.hmcts.net"
+  additional_host_name  = "${local.external_host_name}"
   appinsights_instrumentation_key = "${var.appinsights_instrumentation_key}"
   common_tags = "${var.common_tags}"
 
