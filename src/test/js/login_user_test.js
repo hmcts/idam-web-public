c0bd1d273166e80332ee90fff2158974f7608b32
slonga <38981112+slonga@users.noreply.github.com>
2018-09-07 17:22:37 +0100

    [PREVIEW] SIDM-1634 Added check for self registration enabled in forg… (#77)
    * [PREVIEW] SIDM-1634 Added check for self registration enabled in forgot password flow.
    
    * [PREVIEW] SIDM-1634 Implemented check for client id presence.
    
    * [PREVIEW] SIDM-1661 fixed Unit Tests.
    
    * add waits to test
    
    * test fixes and upgrade electron
    
    * wait for text in header
    
    * added alternative text

--- src/test/js/login_user_test.js
+++ src/test/js/login_user_test.js
@@ -4,6 +4,19 @@ Feature('Login Page');
 
 Scenario('@smoke Login Page', (I) => {
   I.amOnPage('/');
-  I.see('Access Denied');
+  I.waitForText('Access Denied');
   I.seeCurrentUrlEquals('/login');
-});
+}).retry(TestData.SCENARIO_RETRY_LIMIT);
+
+Scenario('@smoke after forgot passwrod you do not see create an account options for invalid clients', (I) => {
+  I.amOnPage(TestData.WEB_PUBLIC_URL + '/login?client_id=zbc&redirect_uri=xyz');
+  I.waitInUrl('/login', 180);
+  I.waitForText('Sign in', 20, 'h1');
+  I.click('Forgotten password?');
+  I.waitInUrl('/reset/forgotpassword', 180);
+  I.fillField('#email', 'resetpasswordtest@mailtest.gov.uk');
+  I.click('Submit');
+  I.waitInUrl('reset/doForgotPassword', 180);
+  I.waitForText('Check your email', 20, 'h1');
+  I.see('If you have entered an email address that is not connected with an account, you will not receive an email. You will need to contact us to create an account');
+}).retry(TestData.SCENARIO_RETRY_LIMIT);
