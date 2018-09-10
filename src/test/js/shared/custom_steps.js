308d7baf9a0066ad97fa5f74d00a129bd315a1b2
Steve Clewer <steve.clewer@amido.com>
2018-09-10 18:01:24 +0100

    Debug for unclicked button

--- src/test/js/shared/custom_steps.js
+++ src/test/js/shared/custom_steps.js
@@ -4,14 +4,12 @@ module.exports = function() {
   return actor({
 
     lockAccount: function (email, serviceName) {
-        var registerPage = TestData.WEB_PUBLIC_URL + '/users/selfRegister?redirect_uri=https://idam.testservice.gov.uk&client_id=' + serviceName;
         var loginPage = TestData.WEB_PUBLIC_URL + '/login?redirect_uri=https://idam.testservice.gov.uk&client_id=' + serviceName + '&state=';
 
         // First
-        this.amOnPage(registerPage);
-        this.click('Sign in to your account');
+        this.amOnPage(loginPage);
         this.waitInUrl('/login', 180);
-        this.waitForText('Sign in or create an account', 20, 'h1');
+        this.dontSee('Incorrect email or password');
         this.see('Sign in');
         this.fillField('username', email);
         this.fillField('password', '111');
@@ -55,6 +53,7 @@ module.exports = function() {
         this.see('Sign in');
         this.fillField('username', email);
         this.fillField('password', '111');
+        this.wait(2);
         this.click('Sign in');
         this.waitForText('There is a problem with your account login details', 20, 'h2');
         this.see('Your account is locked due to too many unsuccessful attempts.');
