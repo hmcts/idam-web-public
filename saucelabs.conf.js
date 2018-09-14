ea2f28e60746a0f06a6baf47a17eb7024cadefbb
AmirAmido <amir.ghahrai@amido.com>
2018-09-14 16:08:44 +0100

    modified so it runs all tests

--- saucelabs.conf.js
+++ saucelabs.conf.js
@@ -2,19 +2,19 @@ const supportedBrowsers = require('./src/test/js/config/supportedBrowsers.js');
 const browser = process.env.SAUCELABS_BROWSER || "chrome_mac_latest";
 const tunnelName = process.env.TUNNEL_IDENTIFIER || 'reformtunnel';
 const setupConfig = {
-    'tests': "./src/test/js/login_user_test.js",
+    'tests': "./src/test/js/*.js",
     'output': './output',
     'timeout': 60000,
     "helpers": {
         "WebDriverIO": {
-            "url": process.env.TEST_URL,
+            "url": "https://idam-web-public.aat.platform.hmcts.net",
             "browser": supportedBrowsers[browser].browserName,
             "cssSelectorsEnabled": "true",
             "ignore-certificate-errors": "true",
             "host": "ondemand.saucelabs.com",
             "port": 80,
-            "user": process.env.SAUCE_USERNAME,
-            "key": process.env.SAUCE_ACCESS_KEY,
+            "user": "idam",
+            "key": "a3a5fa68-4316-4b67-aa9f-3e614b438da7",
             "desiredCapabilities" : getDesiredCapabilities(),
         },
         "idam_helper": {
