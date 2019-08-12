b1359b1781d4b5551e61348fd56cf0e82a6ae2bf
Daniel Patynski <daniel.patynski@amido.com>
2019-08-12 14:59:08 +0100

    SIDM-2869 Change generateRandomText to generate text with no digits.

--- src/test/js/self_registration_test.js
+++ src/test/js/self_registration_test.js
@@ -55,9 +55,9 @@ Scenario('@functional @selfregister User Validation errors', (I) => {
     I.fillField('lastName', '@@');
     I.click('Continue');
     I.wait(5);
-    I.see('Your first name is invalid:');
+    I.see('Your first name is invalid');
     I.see('First name has to be longer than 1 character and should not include digits nor any of these characters:')
-    I.see('Your last name is invalid:');
+    I.see('Your last name is invalid');
     I.see('Last name has to be longer than 1 character and should not include digits nor any of these characters:')
     I.see('Sign in to your account.');
     I.click('Sign in to your account.');
