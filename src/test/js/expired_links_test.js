57f03ccd6388780816a8a4b98dbd577c2056d4d8
Daniel Patynski <daniel.patynski@amido.com>
2019-10-07 15:44:54 +0100

    SIDM-2862 Fix smoke test.

--- src/test/js/expired_links_test.js
+++ src/test/js/expired_links_test.js
@@ -5,7 +5,7 @@ Feature('Error message is displayed when trying to use an expired link');
 Scenario('@smoke @expiredlinks I try to reset my password with an expired reset password link', (I) => {
 
   I.amOnPage('/passwordReset?action=start&token=invalidtoken&code=somecode');
-  I.waitForText('Sorry, your link has expired', 180, 'h1');
-  I.see('Please contact customer services for more information.');
+  I.waitForText('Your link has expired', 180, 'h1');
+  I.see('To get a new link, you need to create your account again');
 
 }).retry(TestData.SCENARIO_RETRY_LIMIT);
