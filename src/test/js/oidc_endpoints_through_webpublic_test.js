8a174b0f23336a6ec297ea3f7aa5bc7b5a1132d7
Henry Dobosn <henrydobson@me.com>
2019-10-08 10:54:20 +0100

    test(oidc): assert against first value in csv

--- src/test/js/oidc_endpoints_through_webpublic_test.js
+++ src/test/js/oidc_endpoints_through_webpublic_test.js
@@ -7,9 +7,9 @@ Scenario('@smoke Verify OpenId connect endpoints config through web public', asy
     let isHttp = TestData.IDAM_API.startsWith("http://");
     let expectedBaseUrl = isHttp ? TestData.WEB_PUBLIC_URL.replace("https://", "http://") : TestData.WEB_PUBLIC_URL;
     let response = await I.getOidcEndPointsConfig(TestData.WEB_PUBLIC_URL);
-    expect(response.authorization_endpoint, 'authorization endpoint').to.equal(expectedBaseUrl+'/o/authorize');
-    expect(response.token_endpoint, 'token endpoint').to.equal(expectedBaseUrl+'/o/token');
-    expect(response.userinfo_endpoint, 'user info endpoint').to.equal(expectedBaseUrl+'/o/userinfo');
-    expect(response.end_session_endpoint, 'end session endpoint').to.equal(expectedBaseUrl+'/o/endSession');
-    expect(response.jwks_uri, 'jwks uri').to.equal(expectedBaseUrl+'/o/jwks');
+    expect(response.authorization_endpoint.rowObject[0], 'authorization endpoint').to.equal(expectedBaseUrl+'/o/authorize');
+    expect(response.token_endpoint.rowObject[0], 'token endpoint').to.equal(expectedBaseUrl+'/o/token');
+    expect(response.userinfo_endpoint.rowObject[0], 'user info endpoint').to.equal(expectedBaseUrl+'/o/userinfo');
+    expect(response.end_session_endpoint.rowObject[0], 'end session endpoint').to.equal(expectedBaseUrl+'/o/endSession');
+    expect(response.jwks_uri.rowObject[0], 'jwks uri').to.equal(expectedBaseUrl+'/o/jwks');
 });
