1dde6f7dc1edb589e9dfe4a88194550e030c85f3
Shravan Mechineni <shravan.mechineni@ladbrokescoral.com>
2019-11-07 12:17:20 +0000

    add mfa otp login tests

---
+++ src/test/js/mfa_e2e_test.js
@@ -0,0 +1,140 @@
+const randomData = require('./shared/random_data');
+const TestData = require('./config/test_data');
+
+Feature('I am able to login with MFA');
+
+let token;
+let adminUserEmail;
+let randomUserFirstName;
+let policyName;
+
+let userFirstNames = [];
+let roleNames = [];
+let serviceNames = [];
+
+const serviceName = randomData.getRandomServiceName();
+
+BeforeSuite(async (I) => {
+    randomUserFirstName = randomData.getRandomUserName();
+    adminUserEmail = randomData.getRandomEmailAddress();
+    policyName = `SIDM_TEST_POLICY_MFA_${randomData.getRandomString()}`;
+
+    token = await I.getAuthToken();
+    let response;
+    response = await I.createRole(randomData.getRandomRoleName() + "_beta", 'beta description', '', token);
+    const serviceBetaRole = response.name;
+    response = await I.createRole(randomData.getRandomRoleName() + "_admin", 'admin description', serviceBetaRole, token);
+    const serviceAdminRole = response.name;
+    response = await I.createRole(randomData.getRandomRoleName() + "_super", 'super description', serviceAdminRole, token);
+    const serviceSuperRole = response.name;
+    const serviceRoles = [serviceBetaRole, serviceAdminRole, serviceSuperRole];
+    roleNames.push(serviceRoles);
+    await I.createServiceWithRoles(serviceName, serviceRoles, serviceBetaRole, token);
+    serviceNames.push(serviceName);
+
+    await I.createUserWithRoles(adminUserEmail, randomUserFirstName, [serviceAdminRole, "IDAM_ADMIN_USER"]);
+    userFirstNames.push(randomUserFirstName);
+
+    await I.createPolicyForMfaTest(policyName, serviceAdminRole, token);
+});
+
+AfterSuite(async (I) => {
+    return Promise.all([
+        I.deleteAllTestData(randomData.TEST_BASE_PREFIX),
+        I.deletePolicy(policyName, token),
+    ]);
+});
+
+Scenario('@functional @mfaLogin I am able to login with MFA', async (I) => {
+    const loginUrl = `${TestData.WEB_PUBLIC_URL}/login?redirect_uri=${TestData.SERVICE_REDIRECT_URI}&client_id=${serviceName}`;
+
+    I.amOnPage(loginUrl);
+    I.waitForText('Sign in', 20, 'h1');
+    I.fillField('#username', adminUserEmail);
+    I.fillField('#password', TestData.PASSWORD);
+    I.click('Sign in');
+    I.wait(10);
+    I.seeInCurrentUrl("/verification");
+    I.waitForText('Verification required', 10, 'h1');
+
+    const otpCode = await I.extractOtpFromEmail(adminUserEmail);
+
+    I.fillField('code', otpCode);
+    I.interceptRequestsAfterSignin();
+    I.click('Submit');
+    I.waitForText(TestData.SERVICE_REDIRECT_URI);
+    I.see('code=');
+    I.dontSee('error=');
+    I.resetRequestInterception();
+}).retry(TestData.SCENARIO_RETRY_LIMIT);
+
+Scenario('@functional @mfaLogin Validate verification code and 3 incorrect otp attempts should redirect user to the sign in page', async (I) => {
+    const loginUrl = `${TestData.WEB_PUBLIC_URL}/login?redirect_uri=${TestData.SERVICE_REDIRECT_URI}&client_id=${serviceName}`;
+
+    I.amOnPage(loginUrl);
+    I.waitForText('Sign in', 20, 'h1');
+    I.fillField('#username', adminUserEmail);
+    I.fillField('#password', TestData.PASSWORD);
+    I.click('Sign in');
+    I.wait(10);
+    I.seeInCurrentUrl("verification");
+    I.waitForText('Verification required', 2, 'h1');
+
+    const otpCode = await I.extractOtpFromEmail(adminUserEmail);
+
+    // empty field
+    I.fillField('code', '');
+    I.click('Submit');
+    I.wait(5);
+    I.see('Enter a verification code');
+    // other than digits
+    I.fillField('code', '663h8w7g');
+    I.click('Submit');
+    I.wait(2);
+    I.see('Enter numbers only');
+    // not 8 digit otp
+    I.fillField('code', `1${otpCode}`);
+    I.click('Submit');
+    I.wait(2);
+    I.see('Enter a valid verification code');
+    // invalid otp
+    I.fillField('code', '12345678');
+    I.click('Submit');
+    I.wait(2);
+    I.see('Verification code incorrect, try again');
+    // invalid otp
+    I.fillField('code', '74646474');
+    I.click('Submit');
+    I.wait(2);
+    I.see('Verification code incorrect, try again');
+
+    // invalid otp
+    I.fillField('code', '94837292');
+    I.click('Submit');
+    I.wait(2);
+    // after 3 incorrect attempts redirect user back to the sing in page
+    I.seeInCurrentUrl(`${TestData.WEB_PUBLIC_URL}/login`);
+    I.see('Verification code check failed');
+    I.see('Your verification code check has failed, please retry');
+    I.fillField('#username', adminUserEmail);
+    I.fillField('#password', TestData.PASSWORD);
+    I.click('Sign in');
+    I.wait(10);
+    I.seeInCurrentUrl("verification");
+    I.waitForText('Verification required', 2, 'h1');
+
+    const otpCodeLatest = await I.extractOtpFromEmail(adminUserEmail);
+
+    // previously generated otp should be invalidated
+    I.fillField('code', otpCode);
+    I.click('Submit');
+    I.wait(5);
+    I.see('Verification code incorrect, try again');
+    I.fillField('code', otpCodeLatest);
+    I.interceptRequestsAfterSignin();
+    I.click('Submit');
+    I.waitForText(TestData.SERVICE_REDIRECT_URI);
+    I.see('code=');
+    I.dontSee('error=');
+    I.resetRequestInterception();
+}).retry(TestData.SCENARIO_RETRY_LIMIT);
