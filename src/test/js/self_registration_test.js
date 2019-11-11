a9db5c51a97c3c7a261f849d4d3d42e3a6b63694
Shravan Mechineni <shravanmechineni5@gmail.com>
2019-11-11 17:01:04 +0000

    Web public tests 1.5 (#267)
    * special character password test
    
    * added password characters
    
    * changed password characters
    
    * redued wait time
    
    * Sidm 3294 mfa e2e tests (#256)
    
    * add mfa otp login tests
    
    * add missing file
    
    * fix typo
    
    * add missing code
    
    * fix test failures
    
    * add more waits
    
    * add more waits
    
    * add exiplit wait
    
    * add block policy scenario

--- src/test/js/self_registration_test.js
+++ src/test/js/self_registration_test.js
@@ -10,6 +10,7 @@ let randomUserFirstName;
 let randomUserLastName;
 let userFirstNames = [];
 let serviceNames = [];
+let specialCharacterPassword;
 
 const selfRegUrl = `${TestData.WEB_PUBLIC_URL}/users/selfRegister?redirect_uri=${TestData.SERVICE_REDIRECT_URI}&client_id=${serviceName}`;
 
@@ -20,6 +21,7 @@ BeforeSuite(async (I) => {
     serviceNames.push(serviceName);
     await I.createUserWithRoles(citizenEmail, randomUserFirstName, ["citizen"]);
     userFirstNames.push(randomUserFirstName);
+    specialCharacterPassword = 'New%%%&&&234';
 });
 
 AfterSuite(async (I) => {
@@ -36,6 +38,7 @@ Scenario('@functional @selfregister User Validation errors', (I) => {
     I.click("Continue");
 
     I.waitForText('Information is missing or invalid', 20, 'h2');
+    I.wait(5);
     I.see('You have not entered your first name');
     I.see('You have not entered your last name');
     I.see('You have not entered your email address');
@@ -113,6 +116,7 @@ Scenario('@functional @selfregister I can self register', async (I) => {
     I.fillField('#password2', TestData.PASSWORD);
     I.click('Continue');
     I.waitForText('Account created', 20, 'h1');
+    I.wait(5);
     I.see('You can now sign in to your account.');
     I.amOnPage(loginPage);
     I.seeInCurrentUrl("state=selfreg");
@@ -121,6 +125,7 @@ Scenario('@functional @selfregister I can self register', async (I) => {
     I.fillField('#password', TestData.PASSWORD);
     I.interceptRequestsAfterSignin();
     I.click('Sign in');
+    I.wait(10);
     I.waitForText(TestData.SERVICE_REDIRECT_URI);
     I.see('code=');
     I.dontSee('error=');
@@ -159,6 +164,7 @@ Scenario('@functional @selfregister @prePopulatedScreen I can self register with
     I.fillField('#password2', TestData.PASSWORD);
     I.click('Continue');
     I.waitForText('Account created', 20, 'h1');
+    I.wait(5);
     I.see('You can now sign in to your account.');
     I.amOnPage(loginPage);
     I.seeInCurrentUrl("state=selfreg");
@@ -167,6 +173,44 @@ Scenario('@functional @selfregister @prePopulatedScreen I can self register with
     I.fillField('#password', TestData.PASSWORD);
     I.interceptRequestsAfterSignin();
     I.click('Sign in');
+    I.waitForText(TestData.SERVICE_REDIRECT_URI);
+    I.see('code=');
+    I.dontSee('error=');
+    I.resetRequestInterception();
+});
+
+Scenario('@functional @selfregister I can self register with repeated special characters in password', async (I) => {
+
+    const email = 'test_citizen.' + randomData.getRandomEmailAddress();
+    const loginPage = `${TestData.WEB_PUBLIC_URL}/login?redirect_uri=${TestData.SERVICE_REDIRECT_URI}&client_id=${serviceName}&state=selfreg`;
+
+    I.amOnPage(selfRegUrl);
+    I.waitInUrl('users/selfRegister', 180);
+    I.waitForText('Create an account or sign in', 20, 'h1');
+    I.see('Create an account');
+    I.fillField('firstName', randomUserFirstName);
+    I.fillField('lastName', randomUserLastName);
+    I.fillField('email', email);
+    I.click("Continue");
+    I.waitForText('Check your email', 20, 'h1');
+    const userActivationUrl = await I.extractUrl(email);
+    I.amOnPage(userActivationUrl);
+    I.waitForText('Create a password', 20, 'h1');
+    I.seeTitleEquals('User Activation - HMCTS Access');
+    I.fillField('#password1', specialCharacterPassword);
+    I.fillField('#password2', specialCharacterPassword);
+    I.click('Continue');
+    I.waitForText('Account created', 20, 'h1');
+    I.wait(10);
+    I.see('You can now sign in to your account.');
+    I.amOnPage(loginPage);
+    I.seeInCurrentUrl("state=selfreg");
+    I.waitForText('Sign in or create an account', 20, 'h1');
+    I.fillField('#username', email);
+    I.fillField('#password', specialCharacterPassword);
+    I.interceptRequestsAfterSignin();
+    I.click('Sign in');
+    I.wait(10);
     I.waitForText(TestData.SERVICE_REDIRECT_URI);
     I.see('code=');
     I.dontSee('error=');
