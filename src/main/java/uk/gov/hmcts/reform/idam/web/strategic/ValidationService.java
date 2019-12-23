2a4dcfb2c50e3dc92d864176535c487452baa352
dfourn <daniel.patynski@amido.com>
2019-12-23 13:04:47 +0000

    Sidm 3530 illegal chars (#299)
    * Add illegal characters validation.
    
    * Add the message.
    
    * Improve test coverage and enhance the for loop.

--- src/main/java/uk/gov/hmcts/reform/idam/web/strategic/ValidationService.java
+++ src/main/java/uk/gov/hmcts/reform/idam/web/strategic/ValidationService.java
@@ -1,19 +1,18 @@
 package uk.gov.hmcts.reform.idam.web.strategic;
 
-import java.io.IOException;
-import java.util.Map;
-import java.util.Objects;
-
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.base.Strings;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.common.base.Strings;
-
 import uk.gov.hmcts.reform.idam.api.internal.model.ErrorResponse;
 import uk.gov.hmcts.reform.idam.web.helper.ErrorHelper;
+
+import javax.annotation.Nonnull;
+import java.io.IOException;
+import java.util.Map;
+import java.util.Objects;
 
 @Component
 public class ValidationService {
@@ -39,10 +38,11 @@ public class ValidationService {
      * @should return false if the passwords is null or empty
      * @should return false if the passwords don't match
      * @should return false if the password does not comply with the length requirement
+     * @should return false if the password contains illegal characters
      * @should return false if both passwords are null or empty
      * @should return true if both passwords are present and comply with the length requirement and are equal
      */
-    public boolean validateResetPasswordRequest(final String password1, final String password2, Map<String, Object> model) {
+    public boolean validatePassword(final String password1, final String password2, Map<String, Object> model) {
 
         if (StringUtils.isEmpty(password1) && StringUtils.isEmpty(password2)) {
             ErrorHelper.showError("Error", "public.common.error.password.not.empty", "public.common.error.enter.password", "public.common.error.enter.password", model);
@@ -71,7 +71,21 @@ public class ValidationService {
             return false;
         }
 
+        if (containsIllegalCharacters(password1)) {
+            ErrorHelper.showError("Error", "public.common.error.invalid.password.illegal-characters", "public.common.error.password.details", "", model);
+            model.put("password1", password1);
+            return false;
+        }
         return true;
+    }
+
+    private boolean containsIllegalCharacters(@Nonnull String password) {
+        for (char c : password.toCharArray()) {
+            if ((c <= '\u001f' && c != '\t') || c >= '\u007f') {
+                return true;
+            }
+        }
+        return false;
     }
 
     /**
