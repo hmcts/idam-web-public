05aae7b813dc74c2214359f3a09b631040b8c373
s.chilamakuri <s.chilamakuri@reply.com>
2020-01-21 11:24:06 +0000

    cbt tests nightly

--- saucelabs.conf.js
+++ saucelabs.conf.js
@@ -1,49 +1,78 @@
+
 const supportedBrowsers = require('./src/test/js/config/supportedBrowsers.js');
-const browser = process.env.SAUCELABS_BROWSER || "ie9_win7";
+//const conf = require('config');
+
+const waitForTimeout = 60000;
+const smartWait = 45000;
+const browser = process.env.SAUCE_BROWSER || 'chrome';
 const tunnelName = process.env.TUNNEL_IDENTIFIER || 'reformtunnel';
-const setupConfig = {
-    'tests': "./src/test/js/*.js",
-    'output': './output',
-    'timeout': 60000,
-    "helpers": {
-        "WebDriverIO": {
-                    "url": process.env.TEST_URL,
-                    "browser": "ie9_win7",
-                    "cssSelectorsEnabled": "true",
-                    "ignore-certificate-errors": "true",
-                    "host": "ondemand.eu-central-1.saucelabs.com",
-                    "port": 80,
-                    "region": "eu",
-                    "user": process.env.SAUCE_USERNAME,
-                    "key": process.env.SAUCE_ACCESS_KEY,
-                    "desiredCapabilities" : getDesiredCapabilities(),
-                    "waitforTimeout": 60000,
-                    "timeouts": {
-                      "script": 60000,
-                      "page load": 60000
-                     }
-                },
-        "idam_helper": {
-            'require': './src/test/js/shared/idam_helper.js'
-        },
-    },
-    'include': {
-        'I': './src/test/js/shared/custom_steps.js'
-    },
-    'mocha': {
-        'reporterOptions': {
-            'reportDir': './output',
-            'reportName' : browser + '_report',
-            'reportTitle': 'Crossbrowser results for: ' + browser.toUpperCase(),
-            'inlineAssets': true
-        }
-    },
-    'name': 'idam-ui-tests'
+
+const getBrowserConfig = browserGroup => {
+  const browserConfig = [];
+  for (const candidateBrowser in supportedBrowsers[browserGroup]) {
+    if (candidateBrowser) {
+      const desiredCapability = supportedBrowsers[browserGroup][candidateBrowser];
+      desiredCapability.tunnelIdentifier = tunnelName;
+      desiredCapability.tags = ['idam-web-punlic'];
+      browserConfig.push({
+        browser: desiredCapability.browserName,
+        desiredCapabilities: desiredCapability
+      });
+    } else {
+      console.error('ERROR: supportedBrowsers.js is empty or incorrectly defined');
+    }
+  }
+  return browserConfig;
 };
-function getDesiredCapabilities() {
-    let desiredCapability = supportedBrowsers[browser];
-    desiredCapability.tunnelIdentifier = tunnelName;
-    desiredCapability.acceptSslCerts = true;
-    return desiredCapability;
-}
- exports.config = setupConfig;
+
+const setupConfig = {
+  tests: './src/test/js/*_.js',
+  output: `${process.cwd()}/functional-output`,
+  helpers: {
+    WebDriverIO: {
+      url: process.env.TEST_URL,
+      browser,
+      waitForTimeout,
+      smartWait,
+      cssSelectorsEnabled: 'true',
+      host: 'ondemand.eu-central-1.saucelabs.com',
+      port: 80,
+      region: 'eu',
+      user: process.env.SAUCE_USERNAME ,
+      key: process.env.SAUCE_ACCESS_KEY,
+      desiredCapabilities: {}
+    },
+    SauceLabsReportingHelper: { require: './src/test/js/shared/sauceLabsReportingHelper.js' },
+
+    IdamHelper: { require: './src/test/js/shared/idam_helper.js' },
+
+  },
+  include: { I: './src/test/js/shared/custom_steps.js' },
+  mocha: {
+    reporterOptions: {
+      reportDir: `${process.cwd()}/functional-output`,
+      reportName: 'index',
+      inlineAssets: true
+    }
+  },
+  multiple: {
+    microsoftIE11: {
+      browsers: getBrowserConfig('microsoftIE11')
+    },
+    microsoftEdge: {
+      browsers: getBrowserConfig('microsoftEdge')
+    },
+    chrome: {
+      browsers: getBrowserConfig('chrome')
+    },
+    firefox: {
+      browsers: getBrowserConfig('firefox')
+    },
+    safari: {
+      browsers: getBrowserConfig('safari')
+    }
+  },
+  name: 'RFE Frontend Tests'
+};
+
+exports.config = setupConfig;
