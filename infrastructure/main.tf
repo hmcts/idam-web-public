f3dcbe57ea977625538ef1e9fcd55bb5d9aaf302
dougbw <doug.brand@amido.com>
2020-06-30 17:20:25 +0100

    feat(redis): provisioning redis infrastructure

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -14,3 +14,40 @@ locals {
   env = "${var.env == "idam-preview" && var.product == "idam" ? "idam-dev" : var.env}"
   tags = "${merge(var.common_tags, map("environment", local.env))}"
 }
+
+data "azurerm_virtual_network" "idam" {
+  name                = "core-infra-vnet-idam-${var.env}"
+  resource_group_name = "core-infra-idam-${var.env}"
+}
+
+data "azurerm_key_vault" "idam" {
+  name                = "${local.vault_name}"
+  resource_group_name = "idam-idam-${var.env}"
+}
+
+module "redis-cache" {
+  source      = "git@github.com:hmcts/cnp-module-redis?ref=master"
+  product     = var.product
+  location    = var.location
+  env         = var.env
+  subnetid    = element(azurerm_virtual_network.idam.subnets, 3)
+  common_tags = var.common_tags
+}
+
+resource "azurerm_key_vault_secret" "redis_hostname" {
+  name         = "redis-hostname"
+  value        = module.redis-cache.host_name
+  key_vault_id = data.azurerm_key_vault.default.id
+}
+
+resource "azurerm_key_vault_secret" "redis_port" {
+  name         = "redis-hostname"
+  value        = module.redis-cache.redis_port
+  key_vault_id = data.azurerm_key_vault.default.id
+}
+
+resource "azurerm_key_vault_secret" "redis_access_key" {
+  name         = "redis-key"
+  value        = module.redis-cache.access_key
+  key_vault_id = data.azurerm_key_vault.default.id
+}
