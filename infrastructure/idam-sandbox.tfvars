a16ab29f47e4130cacfab6071d6432fc59c773c5
Henry Dobson <henrydobson@me.com>
2020-07-09 16:50:54 +0100

    chore(tf): deprecations

--- infrastructure/idam-sandbox.tfvars
+++
@@ -1,4 +0,0 @@
-idam_api_url_override = "http://idam-api-idam-sandbox.service.core-compute-idam-sandbox.internal"
-ssl_verification_enabled = "false"
-https_only = "false"
-capacity = 1
--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,19 +1,18 @@
+terraform {
+  backend "azurerm" {}
+}
+
 provider "azurerm" {
   version = "~> 2.16.0"
   features {}
 }
 
 locals {
-  default_vault_name = "${var.product}-${var.env}"
-  vault_name = "${coalesce(var.vault_name_override, local.default_vault_name)}"
-  vault_uri = "https://${local.vault_name}.vault.azure.net/"
+  default_vault_name         = "${var.product}-${var.env}"
+  vault_uri                  = "https://${local.default_vault_name}.vault.azure.net/"
   default_external_host_name = "idam-web-public.${replace(var.env, "idam-", "")}.platform.hmcts.net"
-  external_host_name = "${var.external_host_name_override != "" ? var.external_host_name_override : local.default_external_host_name}"
-  default_idam_api = "https://idam-api.${replace(var.env, "idam-", "")}.platform.hmcts.net"
-  idam_api_url = "${var.idam_api_url_override != "" ? var.idam_api_url_override : local.default_idam_api}"
-  idam_api_testing_support_url = "${var.idam_api_testing_support_url_override != "" ? var.idam_api_testing_support_url_override : local.idam_api_url}"
-  env = "${var.env == "idam-preview" && var.product == "idam" ? "idam-dev" : var.env}"
-  tags = "${merge(var.common_tags, map("environment", local.env))}"
+  env                        = "${var.env == "idam-preview" && var.product == "idam" ? "idam-dev" : var.env}"
+  tags                       = "${merge(var.common_tags, map("environment", local.env))}"
 }
 
 data "azurerm_virtual_network" "idam" {
