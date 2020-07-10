a976271b50ffcec4babb5edd6a41b6fbbf3ce24f
Shravan Mechineni <shravan.mechineni@ladbrokescoral.com>
2020-07-10 15:04:07 +0100

    add stale user login in welsh

--- src/test/js/stale_user_login_test.js
+++ src/test/js/stale_user_login_test.js
@@ -1,5 +1,6 @@
 const testData = require('./config/test_data');
 const randomData = require('./shared/random_data');
+const Welsh = require('./shared/welsh_constants');
 
 Feature('Stale user login');
 
@@ -46,14 +47,15 @@ Scenario('@functional @staleUserLogin Stale user login journey', async(I) => {
     I.click('Sign in');
     I.wait(5);
     I.waitForText('You need to reset your password', 20, 'h2');
+    I.waitForText('As you\'ve not logged in for at least 90 days, you need to reset your password.', 20);
     const reRegistrationUrl = await I.extractUrl(staleUserEmail);
     I.amOnPage(reRegistrationUrl);
-    I.waitForText('Create a password', 20, 'h1');
+    I.waitForText('Create a new password', 20, 'h1');
     I.fillField('#password1', testData.PASSWORD);
     I.fillField('#password2', testData.PASSWORD);
     I.click('Continue');
-    I.waitForText('Account created', 20, 'h1');
-    I.waitForText('You can now sign in to your account.', 20);
+    I.waitForText('Your password has been changed', 20, 'h1');
+    I.see('You can now sign in with your new password.');
 
     I.amOnPage(loginUrl);
     I.waitForText('Sign in', 20, 'h1');
@@ -61,6 +63,39 @@ Scenario('@functional @staleUserLogin Stale user login journey', async(I) => {
     I.fillField('#password', testData.PASSWORD);
     I.interceptRequestsAfterSignin();
     I.click('Sign in');
+    I.waitForText(testData.SERVICE_REDIRECT_URI);
+    I.see('code=');
+    I.dontSee('error=');
+    I.resetRequestInterception();
+});
+
+
+Scenario('@functional @staleUserLogin @Welsh Stale user login journey in welsh', async(I) => {
+    const loginUrl = `${testData.WEB_PUBLIC_URL}/login?redirect_uri=${testData.SERVICE_REDIRECT_URI}&client_id=${serviceName}${Welsh.urlForceCy}`;
+
+    I.amOnPage(loginUrl);
+    I.waitForText(Welsh.signIn, 20, 'h1');
+    I.fillField('#username', staleUserEmail);
+    I.fillField('#password', testData.PASSWORD);
+    I.click(Welsh.signIn);
+    I.wait(5);
+    I.waitForText(Welsh.youNeedToResetYourPassword, 20, 'h2');
+    I.waitForText(Welsh.staleUserErrorMessage, 20);
+    const reRegistrationUrl = await I.extractUrl(staleUserEmail);
+    I.amOnPage(reRegistrationUrl);
+    I.waitForText(Welsh.createANewPassword, 20, 'h1');
+    I.fillField('#password1', testData.PASSWORD);
+    I.fillField('#password2', testData.PASSWORD);
+    I.click(Welsh.continueBtn);
+    I.waitForText(Welsh.passwordChanged, 20, 'h1');
+    I.waitForText(Welsh.youCanNowSignInWithYourNewPassword);
+
+    I.amOnPage(loginUrl);
+    I.waitForText(Welsh.signIn, 20, 'h1');
+    I.fillField('#username', staleUserEmail);
+    I.fillField('#password', testData.PASSWORD);
+    I.interceptRequestsAfterSignin();
+    I.click(Welsh.signIn);
     I.waitForText(testData.SERVICE_REDIRECT_URI);
     I.see('code=');
     I.dontSee('error=');
