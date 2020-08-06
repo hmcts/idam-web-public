a223c600131077d0800d78200314be2cafec5fc1
Nikola Naydenov <nikolanaydenov@aml0232.home>
2020-08-06 13:20:38 +0100

    New Pr

--- src/test/java/uk/gov/hmcts/reform/idam/web/config/EmbeddedRedis.java
+++ src/test/java/uk/gov/hmcts/reform/idam/web/config/EmbeddedRedis.java
@@ -3,7 +3,6 @@ package uk.gov.hmcts.reform.idam.web.config;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.test.context.TestPropertySource;
 
 @TestConfiguration
 public class EmbeddedRedis {
