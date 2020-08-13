f550815e58c3f6c7ac6faf9fda42ef9167262785
Tiago Braun <tiago.braun@amido.com>
2020-08-13 12:31:26 +0100

    chore(merge-master): AppConfiguration: revert to preview version

--- src/main/java/uk/gov/hmcts/reform/idam/web/config/AppConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/idam/web/config/AppConfiguration.java
@@ -6,17 +6,28 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.ssl.SSLContexts;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
+import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
+import org.springframework.security.oauth2.core.OAuth2TokenValidator;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.client.RestTemplate;
+import uk.gov.hmcts.reform.idam.web.client.OidcApi;
+import uk.gov.hmcts.reform.idam.web.client.SsoFederationApi;
 import uk.gov.hmcts.reform.idam.web.config.properties.ConfigurationProperties;
 import uk.gov.hmcts.reform.idam.web.helper.LocalePassingInterceptor;
+import uk.gov.hmcts.reform.idam.web.sso.SSOAuthenticationSuccessHandler;
 
 import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.SSLContext;
@@ -29,8 +40,25 @@ import java.util.concurrent.TimeUnit;
 @Configuration
 public class AppConfiguration extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    private ConfigurationProperties configurationProperties;
+    private final ConfigurationProperties configurationProperties;
+
+    private final OAuth2AuthorizedClientRepository repository;
+
+    private final SsoFederationApi ssoFederationApi;
+
+    private final OidcApi oidcApi;
+
+    @Value("${spring.security.oauth2.client.provider.oidc.issuer-uri}")
+    private String issuerUri;
+
+    public AppConfiguration(ConfigurationProperties configurationProperties,
+                            OAuth2AuthorizedClientRepository repository,
+                            SsoFederationApi ssoFederationApi, OidcApi oidcApi) {
+        this.configurationProperties = configurationProperties;
+        this.repository = repository;
+        this.ssoFederationApi = ssoFederationApi;
+        this.oidcApi = oidcApi;
+    }
 
     @Bean
     public RestTemplate getRestTemplate()
@@ -72,17 +100,50 @@ public class AppConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        // @formatter:off
         http
-            .csrf().ignoringAntMatchers("/o/**")
+            .csrf()
+                .ignoringAntMatchers("/o/**")
             .csrfTokenRepository(new CookieCsrfTokenRepository()).and()
             .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
             .authorizeRequests()
-            .antMatchers("/o/**").permitAll()
-            .antMatchers("/resources/**").permitAll()
-            .antMatchers("/assets/**").permitAll()
-            .antMatchers("/users/register").permitAll()
-            .antMatchers("/users/activate").permitAll()
-            .anyRequest()
-            .permitAll();
+                .antMatchers("/o/**").permitAll()
+                .antMatchers("/resources/**").permitAll()
+                .antMatchers("/assets/**").permitAll()
+                .antMatchers("/users/register").permitAll()
+                .antMatchers("/users/activate").permitAll()
+                .anyRequest().permitAll()
+            .and()
+                .oauth2Login()
+                    // non existent page otherwise defaults to /login
+                    .loginPage("/sso/login.html")
+                    .failureUrl("/error")
+                    .successHandler(myAuthenticationSuccessHandler(repository))
+            .and()
+                .oauth2ResourceServer()
+            .jwt()
+            .and()
+            .and()
+                .oauth2Client();
+        // @formatter:on
+    }
+
+    @Bean
+    public AuthenticationSuccessHandler myAuthenticationSuccessHandler(OAuth2AuthorizedClientRepository repository){
+        return new SSOAuthenticationSuccessHandler(repository, ssoFederationApi, oidcApi,
+            configurationProperties.getStrategic().getSession());
+    }
+
+     /*
+      * If in future we have multiple providers this needs to be replaced with spring security 5s
+      * JwtIssuerAuthenticationManagerResolver which supports multiple issuers
+      */
+    @Bean
+    JwtDecoder jwtDecoder() {
+        NimbusJwtDecoder jwtDecoder = (NimbusJwtDecoder) JwtDecoders.fromOidcIssuerLocation(issuerUri);
+        OAuth2TokenValidator<Jwt> withAudience = new DelegatingOAuth2TokenValidator<>();
+        jwtDecoder.setJwtValidator(withAudience);
+
+        return jwtDecoder;
     }
 }
