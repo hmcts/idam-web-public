4ee839d5f6977df1d9a227fe134b356859ac2866
Radoslaw Orlowski <radoslaw.orlowski@amido.com>
2020-08-17 14:51:49 +0100

    SIDM-4641 Disable SSO login button when the feature flag is off.

--- src/main/java/uk/gov/hmcts/reform/idam/web/AppController.java
+++ src/main/java/uk/gov/hmcts/reform/idam/web/AppController.java
@@ -338,6 +338,7 @@ public class AppController {
         if (request.isAzureLoginEnabled()) {
             model.addAttribute(AZURE_LOGIN_ENABLED, true);
         }
+        model.addAttribute(SSO_ENABLED, configurationProperties.getFeatures().isFederatedSSO());
 
         final boolean validationErrors = bindingResult.hasErrors();
         if (validationErrors) {
