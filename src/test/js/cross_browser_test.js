37124a12a7e7ea90b1d5ea3a5e2b72c41c664ddb
shravan mechineni <shravanmechineni5@gmail.com>
2020-08-26 11:33:33 +0100

    cross browser tests update

--- src/test/js/cross_browser_test.js
+++ src/test/js/cross_browser_test.js
@@ -1,5 +1,3 @@
-const chai = require('chai');
-const {expect} = chai;
 const TestData = require('./config/test_data');
 const randomData = require('./shared/random_data');
 
@@ -11,10 +9,6 @@ let userFirstNames = [];
 let roleNames = [];
 let serviceNames = [];
 let randomUserLastName;
-let existingCitizenEmail;
-let pinUserFirstName;
-let pinUserLastName;
-let pinaccessToken;
 
 const serviceName = randomData.getRandomServiceName();
 
@@ -24,9 +18,6 @@ BeforeSuite(async (I) => {
     randomUserFirstName = randomData.getRandomUserName();
     const adminEmail = 'admin.' + randomData.getRandomEmailAddress();
     citizenEmail = 'citizen.' + randomData.getRandomEmailAddress();
-    existingCitizenEmail = 'existingcitizen.' + randomData.getRandomEmailAddress();
-    pinUserLastName = randomData.getRandomUserName() + 'pinępinç';
-    pinUserFirstName = randomData.getRandomUserName() + 'ępinçłpin';
 
     let token = await I.getAuthToken();
     let response;
@@ -45,26 +36,18 @@ BeforeSuite(async (I) => {
     await I.createUserWithRoles(citizenEmail, randomUserFirstName + 'Citizen', ["citizen"]);
     userFirstNames.push(randomUserFirstName + 'Citizen');
     randomUserLastName = randomData.getRandomUserName();
-    await I.createUserWithRoles(citizenEmail, randomUserFirstName, ["citizen"]);
-    userFirstNames.push(randomUserFirstName);
-    await I.createUserWithRoles(existingCitizenEmail, randomUserFirstName + 'Citizen', ["citizen"]);
-        userFirstNames.push(randomUserFirstName + 'Citizen');
-
-     const pinUser = await I.getPinUser(pinUserFirstName, pinUserLastName);
-     const code = await I.loginAsPin(pinUser.pin, serviceName, TestData.SERVICE_REDIRECT_URI);
-     pinaccessToken = await I.getAccessToken(code, serviceName, TestData.SERVICE_REDIRECT_URI, TestData.SERVICE_CLIENT_SECRET);
 });
 
 AfterSuite(async (I) => {
      return I.deleteAllTestData(randomData.TEST_BASE_PREFIX)
 });
 
-Scenario('@functional @crossbrowser Idam Web public cross browser tests', async (I) => {
-
-    const email = 'test_citizen.' + randomData.getRandomEmailAddress();
+Scenario('@crossbrowser Idam Web public cross browser tests', async (I) => {
     citizenEmail = 'citizen.' + randomData.getRandomEmailAddress();
 
-    const loginPage = `${TestData.WEB_PUBLIC_URL}/login?redirect_uri=${TestData.SERVICE_REDIRECT_URI}&client_id=${serviceName}&state=selfreg`;
+    const loginPage = `${TestData.WEB_PUBLIC_URL}/login?redirect_uri=${TestData.SERVICE_REDIRECT_URI}&client_id=${serviceName}`;
+
+    // create account
     I.amOnPage(selfRegUrl);
     I.waitInUrl('users/selfRegister', 180);
     I.waitForText('Create an account or sign in', 20, 'h1');
@@ -84,9 +67,21 @@ Scenario('@functional @crossbrowser Idam Web public cross browser tests', async 
     I.click('Continue');
     I.waitForText('Account created', 20, 'h1');
     I.see('You can now sign in to your account.');
-    I.wait(5);
-    I.lockAccount(citizenEmail, serviceName);
-    I.click('reset your password');
+
+    // login
+    I.amOnPage(loginPage);
+    I.waitForText('Sign in', 20, 'h1');
+    I.fillField('#username', ' ' + citizenEmail);
+    I.fillField('#password', TestData.PASSWORD);
+    I.click('Sign in');
+    I.waitForText(TestData.SERVICE_REDIRECT_URI);
+    I.see('code=');
+    I.dontSee('error=');
+
+    //Reset password
+    I.amOnPage(loginPage);
+    I.waitForText('Sign in or create an account', 20, 'h1');
+    I.click('Forgotten password?');
     I.waitForText('Reset your password', 20, 'h1');
     I.fillField('#email', citizenEmail);
     I.click('Submit');
@@ -101,19 +96,4 @@ Scenario('@functional @crossbrowser Idam Web public cross browser tests', async 
     I.click('Continue');
     I.waitForText('Your password has been changed', 20, 'h1');
     I.see('You can now sign in with your new password.');
-
-    const userInfo =  await I.retry({retries: 3, minTimeout: 10000}).getUserByEmail(citizenEmail);
-      expect(userInfo.active).to.equal(true);
-      expect(userInfo.email).to.equal(citizenEmail);
-      expect(userInfo.forename).to.equal(randomUserFirstName);
-      expect(userInfo.id).to.not.equal(null);
-      expect(userInfo.roles).to.eql(['citizen']);
-
-    I.amOnPage(`${TestData.WEB_PUBLIC_URL}/login/uplift?client_id=${serviceName}&redirect_uri=${TestData.SERVICE_REDIRECT_URI}&jwt=${pinaccessToken}`);
-    I.waitForText('Sign in to your account.', 30);
-    I.click('Sign in to your account.');
-    I.fillField('#username', existingCitizenEmail);
-    I.wait(2)
-    I.fillField('#password', TestData.PASSWORD);
-    I.click('Sign in');
 });
