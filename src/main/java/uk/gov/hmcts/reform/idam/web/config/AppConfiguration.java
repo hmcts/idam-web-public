a6709d411431f926e0dc245ca89b08431cb1d323
Radoslaw Orlowski <radoslaw.orlowski@amido.com>
2020-09-10 13:09:29 +0100

    Temporarily delete the unused bean.

--- src/main/java/uk/gov/hmcts/reform/idam/web/config/AppConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/idam/web/config/AppConfiguration.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.reform.idam.web.config;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -8,7 +7,6 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
-@ConditionalOnMissingBean(AppConfigurationSSO.class)
 public class AppConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
@@ -16,16 +14,16 @@ public class AppConfiguration extends WebSecurityConfigurerAdapter {
         // @formatter:off
         http
             .csrf()
-                .ignoringAntMatchers("/o/**")
+            .ignoringAntMatchers("/o/**")
             .csrfTokenRepository(new CookieCsrfTokenRepository()).and()
             .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
             .authorizeRequests()
-                .antMatchers("/o/**").permitAll()
-                .antMatchers("/resources/**").permitAll()
-                .antMatchers("/assets/**").permitAll()
-                .antMatchers("/users/register").permitAll()
-                .antMatchers("/users/activate").permitAll()
-                .anyRequest().permitAll();
+            .antMatchers("/o/**").permitAll()
+            .antMatchers("/resources/**").permitAll()
+            .antMatchers("/assets/**").permitAll()
+            .antMatchers("/users/register").permitAll()
+            .antMatchers("/users/activate").permitAll()
+            .anyRequest().permitAll();
         // @formatter:on
     }
 
