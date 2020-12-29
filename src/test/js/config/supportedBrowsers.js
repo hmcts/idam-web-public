da798ae5f429d5d284a1597057d05f001fa6dd98
shravan mechineni <shravanmechineni5@gmail.com>
2020-12-29 10:55:18 +0000

    add IE and Safari in cross browser list

--- src/test/js/config/supportedBrowsers.js
+++ src/test/js/config/supportedBrowsers.js
@@ -1,32 +1,76 @@
+const LATEST_MAC = 'macOS 10.15';
+const LATEST_WINDOWS = 'Windows 10';
+
 const supportedBrowsers = {
-  chrome: {
-    chrome_win_latest: {
-      browserName: 'chrome',
-      name: 'DIV_WIN_CHROME_LATEST',
-      platform: 'Windows 10',
-      version: 'latest'
+    microsoftIE11: {
+        ie11: {
+            browserName: 'internet explorer',
+            platformName: LATEST_WINDOWS,
+            browserVersion: 'latest',
+            'sauce:options': {
+                name: 'IE11',
+                screenResolution: '1400x1050'
+            }
+        }
     },
-    chrome_mac_latest: {
-      browserName: 'chrome',
-      name: 'MAC_CHROME_LATEST',
-      platform: 'macOS 10.13',
-      version: 'latest'
-    }
-  },
-  firefox: {
-    firefox_win_latest: {
-      browserName: 'firefox',
-      name: 'WIN_FIREFOX_LATEST',
-      platform: 'Windows 10',
-      version: 'latest'
+    microsoftEdge: {
+        edge: {
+            browserName: 'MicrosoftEdge',
+            platformName: LATEST_WINDOWS,
+            browserVersion: 'latest',
+            'sauce:options': {
+                name: 'Edge_Win10'
+            }
+        }
     },
-    firefox_mac_latest: {
-      browserName: 'firefox',
-      name: 'MAC_FIREFOX_LATEST',
-      platform: 'macOS 10.13',
-      version: 'latest'
+    safari: {
+        safari_mac_latest: {
+            browserName: 'safari',
+            platformName: 'macOS 10.14',
+            browserVersion: 'latest',
+            'sauce:options': {
+                name: 'MAC_SAFARI_LATEST',
+                seleniumVersion: '3.141.59',
+                screenResolution: '1400x1050'
+            }
+        }
+    },
+    chrome: {
+        chrome_win_latest: {
+            browserName: 'chrome',
+            platformName: LATEST_WINDOWS,
+            browserVersion: 'latest',
+            'sauce:options': {
+                name: 'WIN_CHROME_LATEST'
+            }
+        },
+        chrome_mac_latest: {
+            browserName: 'chrome',
+            platformName: LATEST_MAC,
+            browserVersion: 'latest',
+            'sauce:options': {
+                name: 'MAC_CHROME_LATEST'
+            }
+        }
+    },
+    firefox: {
+        firefox_win_latest: {
+            browserName: 'firefox',
+            platformName: LATEST_WINDOWS,
+            browserVersion: 'latest',
+            'sauce:options': {
+                name: 'WIN_FIREFOX_LATEST'
+            }
+        },
+        firefox_mac_latest: {
+            browserName: 'firefox',
+            platformName: LATEST_MAC,
+            browserVersion: 'latest',
+            'sauce:options': {
+                name: 'MAC_FIREFOX_LATEST'
+            }
+        }
     }
-  }
 };
 
 module.exports = supportedBrowsers;
