bb20d2096f068a9521b111efda7fcbab97f3dc82
shravan mechineni <shravanmechineni5@gmail.com>
2021-01-28 14:32:29 +0000

    update WebDriverIO helper to WebDriver

--- src/test/js/shared/sauceLabsReportingHelper.js
+++ src/test/js/shared/sauceLabsReportingHelper.js
@@ -18,13 +18,13 @@ function updateSauceLabsResult(result, sessionId) {
 module.exports = function() {
   // Setting test success on SauceLabs
   event.dispatcher.on(event.test.passed, () => {
-    const sessionId = container.helpers('WebDriverIO').browser.requestHandler.sessionID;
+    const sessionId = container.helpers('WebDriver').browser.requestHandler.sessionID;
     exec(updateSauceLabsResult('true', sessionId));
   });
 
   // Setting test failure on SauceLabs
   event.dispatcher.on(event.test.failed, () => {
-    const sessionId = container.helpers('WebDriverIO').browser.requestHandler.sessionID;
+    const sessionId = container.helpers('WebDriver').browser.requestHandler.sessionID;
     exec(updateSauceLabsResult('false', sessionId));
   });
 };
