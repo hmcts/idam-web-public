7de209fd43bc6d03e53668036b39a4945a729dc6
James Burke (IDAM) <james.burke@hmcts.net>
2021-02-01 17:35:28 +0000

    update config

--- saucelabs.conf.js
+++ saucelabs.conf.js
@@ -5,17 +5,24 @@ const tunnelName = process.env.TUNNEL_IDENTIFIER || 'reformtunnel';
 const waitForTimeout = 60000;
 const smartWait = 5000;
 
+const defaultSauceOptions = {
+  username: process.env.SAUCE_USERNAME,
+  accessKey: process.env.SAUCE_ACCESS_KEY,
+  tunnelIdentifier: process.env.TUNNEL_IDENTIFIER || 'reformtunnel',
+  acceptSslCerts: true,
+  tags: ['idam-web-public']
+};
+
 const getBrowserConfig = browserGroup => {
     const browserConfig = [];
     for (const candidateBrowser in supportedBrowsers[browserGroup]) {
         if (candidateBrowser) {
-            const desiredCapability = supportedBrowsers[browserGroup][candidateBrowser];
-            desiredCapability.acceptSslCerts = true;
-            desiredCapability.tunnelIdentifier = tunnelName;
-            desiredCapability.tags = ['idam-web-public'];
+            console.log("Setting up " + browserGroup)
+            const candidateCapabilities = supportedBrowsers[browserGroup][candidateBrowser];
+            candidateCapabilities['sauce:options'] = merge(defaultSauceOptions, candidateCapabilities['sauce:options']);
             browserConfig.push({
-                browser: desiredCapability.browserName,
-                desiredCapabilities: desiredCapability
+                browser: candidateCapabilities.browserName,
+                capabilities: candidateCapabilities
             });
         } else {
             console.error('ERROR: supportedBrowsers.js is empty or incorrectly defined');
@@ -23,6 +30,10 @@ const getBrowserConfig = browserGroup => {
     }
     return browserConfig;
 };
+
+function merge(intoObject, fromObject) {
+  return Object.assign({}, intoObject, fromObject);
+}
 
 const setupConfig = {
     tests: './src/test/js/cross_browser_test.js',
@@ -37,12 +48,8 @@ const setupConfig = {
             host: 'ondemand.eu-central-1.saucelabs.com',
             port: 80,
             region: 'eu',
-            sauceConnect: true,
-            services: ['sauce'],
-            acceptSslCerts: true,
-            user: process.env.SAUCE_USERNAME,
-            key: process.env.SAUCE_ACCESS_KEY,
-            desiredCapabilities: {}
+            capabilities: {}
+
         },
         SauceLabsReportingHelper: {require: './src/test/js/shared/sauceLabsReportingHelper.js'},
         idam_helper: {require: './src/test/js/shared/idam_helper.js'}
--- src/test/js/config/supportedBrowsers.js
+++ src/test/js/config/supportedBrowsers.js
@@ -1,43 +1,74 @@
+const LATEST_MAC = 'macOS 10.15';
+const LATEST_WINDOWS = 'Windows 10';
+
 const supportedBrowsers = {
-    microsoft: {
-        edge: {
-            browserName: 'MicrosoftEdge',
-            name: 'Edge',
-            platform: 'Windows 10',
-            ignoreZoomSetting: true,
-            nativeEvents: false,
-            ignoreProtectedModeSettings: true,
-            version: '17.17134'
-        }
+  microsoft: {
+    ie11: {
+      browserName: 'internet explorer',
+      platformName: LATEST_WINDOWS,
+      browserVersion: 'latest',
+      'sauce:options': {
+        name: 'IE11_RESPONDENT',
+        screenResolution: '1400x1050'
+      }
     },
-    chrome: {
-        chrome_win_latest: {
-            browserName: 'chrome',
-            name: 'WIN_CHROME_LATEST',
-            platform: 'Windows 10',
-            version: 'latest'
-        },
-        chrome_mac_latest: {
-            browserName: 'chrome',
-            name: 'MAC_CHROME_LATEST',
-            platform: 'macOS 10.13',
-            version: 'latest'
-        }
-    },
-    firefox: {
-        firefox_win_latest: {
-            browserName: 'firefox',
-            name: 'WIN_FIREFOX_LATEST',
-            platform: 'Windows 10',
-            version: 'latest'
-        },
-        firefox_mac_latest: {
-            browserName: 'firefox',
-            name: 'MAC_FIREFOX_LATEST',
-            platform: 'macOS 10.13',
-            version: 'latest'
-        }
+    edge: {
+      browserName: 'MicrosoftEdge',
+      platformName: LATEST_WINDOWS,
+      browserVersion: 'latest',
+      'sauce:options': {
+        name: 'Edge_Win10_RESPONDENT'
+      }
     }
+  },
+  safari: {
+    safari_mac_latest: {
+      browserName: 'safari',
+      platformName: 'macOS 10.14',
+      browserVersion: 'latest',
+      'sauce:options': {
+        name: 'MAC_SAFARI_LATEST_RESPONDENT',
+        seleniumVersion: '3.141.59',
+        screenResolution: '1400x1050'
+      }
+    }
+  },
+  chrome: {
+    chrome_win_latest: {
+      browserName: 'chrome',
+      platformName: LATEST_WINDOWS,
+      browserVersion: 'latest',
+      'sauce:options': {
+        name: 'WIN_CHROME_LATEST_RESPONDENT'
+      }
+    },
+    chrome_mac_latest: {
+      browserName: 'chrome',
+      platformName: LATEST_MAC,
+      browserVersion: 'latest',
+      'sauce:options': {
+        name: 'MAC_CHROME_LATEST_RESPONDENT'
+      }
+    }
+  },
+  firefox: {
+    firefox_win_latest: {
+      browserName: 'firefox',
+      platformName: LATEST_WINDOWS,
+      browserVersion: 'latest',
+      'sauce:options': {
+        name: 'WIN_FIREFOX_LATEST_RESPONDENT'
+      }
+    },
+    firefox_mac_latest: {
+      browserName: 'firefox',
+      platformName: LATEST_MAC,
+      browserVersion: 'latest',
+      'sauce:options': {
+        name: 'MAC_FIREFOX_LATEST_RESPONDENT'
+      }
+    }
+  }
 };
 
 module.exports = supportedBrowsers;
