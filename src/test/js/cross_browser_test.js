196f8f2d69ad34471c9559e3d57a1e35ca39e42b
Shravan Mechineni <shravanmechineni5@gmail.com>
2021-04-28 13:53:18 +0100

    Sidm 5692 cross browser tests config update (#604)
    * update cross browser tests and config
    
    * add plugins to retry and auto delay
    
    * improve logs, reporting and rerun

--- src/test/js/cross_browser_test.js
+++ src/test/js/cross_browser_test.js
@@ -4,7 +4,9 @@ const randomData = require('./shared/random_data');
 Feature('Users can create account, sign in and reset password');
 
 let randomUserFirstName;
-let citizenEmail;
+let citizenUserSelfRegistrationEmail;
+let citizenUserLoginEmail;
+let citizenUserPasswordResetEmail;
 let userFirstNames = [];
 let serviceNames = [];
 let randomUserLastName;
@@ -14,37 +16,38 @@ const serviceClientSecret = randomData.getRandomClientSecret();
 const userPassword = randomData.getRandomUserPassword();
 
 const selfRegUrl = `${TestData.WEB_PUBLIC_URL}/users/selfRegister?redirect_uri=${TestData.SERVICE_REDIRECT_URI}&client_id=${serviceName}`;
+const loginPage = `${TestData.WEB_PUBLIC_URL}/login?redirect_uri=${TestData.SERVICE_REDIRECT_URI}&client_id=${serviceName}`;
 
 BeforeSuite(async ({ I }) => {
     randomUserFirstName = randomData.getRandomUserName();
     randomUserLastName = randomData.getRandomUserName();
-    citizenEmail = 'citizen.' + randomData.getRandomEmailAddress();
+    citizenUserLoginEmail = 'citizenLogin' + randomData.getRandomEmailAddress();
 
     await I.createServiceData(serviceName, serviceClientSecret);
     serviceNames.push(serviceName);
 
-    userFirstNames.push(randomUserFirstName);
+    await I.createUserWithRoles(citizenUserLoginEmail, userPassword, randomUserFirstName + 'citizenLogin', ["citizen"]);
+    userFirstNames.push(randomUserFirstName + 'citizenSelfreg');
+    userFirstNames.push(randomUserFirstName + 'citizenLogin');
+    userFirstNames.push(randomUserFirstName + 'citizenPasswordReset');
 });
 
 AfterSuite(async ({ I }) => {
      return I.deleteAllTestData(randomData.TEST_BASE_PREFIX)
 });
 
-Scenario('@crossbrowser Idam Web public cross browser tests', async ({ I }) => {
-    const loginPage = `${TestData.WEB_PUBLIC_URL}/login?redirect_uri=${TestData.SERVICE_REDIRECT_URI}&client_id=${serviceName}`;
-    const resetPassword = randomData.getRandomUserPassword();
-
-    // create account
+Scenario('@crossbrowser Citizen user self registration', async ({ I }) => {
+    citizenUserSelfRegistrationEmail = 'citizenSelfreg' + randomData.getRandomEmailAddress();
     I.amOnPage(selfRegUrl);
     I.waitInUrl('users/selfRegister');
     I.waitForText('Create an account or sign in');
     I.see('Create an account');
-    I.fillField('firstName', randomUserFirstName);
+    I.fillField('firstName', randomUserFirstName + 'citizenSelfreg');
     I.fillField('lastName', randomUserLastName);
-    I.fillField('email', citizenEmail);
+    I.fillField('email', citizenUserSelfRegistrationEmail);
     I.click("Continue");
     I.waitForText('Check your email');
-    const userActivationUrl = await I.extractUrlFromNotifyEmail(citizenEmail);
+    const userActivationUrl = await I.extractUrlFromNotifyEmail(citizenUserSelfRegistrationEmail);
     I.amOnPage(userActivationUrl);
     I.waitForText('Create a password');
     I.seeTitleEquals('User Activation - HMCTS Access');
@@ -52,24 +55,30 @@ Scenario('@crossbrowser Idam Web public cross browser tests', async ({ I }) => {
     I.fillField('#password2', userPassword);
     I.click('Continue');
     I.waitForText('Account created');
+}).retry(TestData.SCENARIO_RETRY_LIMIT);
 
-    // login
+Scenario('@crossbrowser Citizen user login', async ({ I }) => {
     I.amOnPage(loginPage);
     I.waitForText('Sign in');
-    I.fillField('#username', citizenEmail);
+    I.fillField('#username', citizenUserLoginEmail);
     I.fillField('#password', userPassword);
     I.click('Sign in');
     I.waitForInvisible('#username', 20);
+}).retry(TestData.SCENARIO_RETRY_LIMIT);
 
-    //Reset password
+Scenario('@crossbrowser Citizen user password reset', async ({ I }) => {
+    citizenUserPasswordResetEmail = 'citizenPasswordReset' + randomData.getRandomEmailAddress();
+    await I.createUserWithRoles(citizenUserPasswordResetEmail, userPassword, randomUserFirstName + 'citizenPasswordReset', ["citizen"]);
+    const resetPassword = randomData.getRandomUserPassword();
+
     I.amOnPage(loginPage);
     I.waitForText('Sign in or create an account');
     I.click('Forgotten password?');
     I.waitForText('Reset your password');
-    I.fillField('#email', citizenEmail);
+    I.fillField('#email', citizenUserPasswordResetEmail);
     I.click('Submit');
     I.waitForText('Check your email');
-    const resetPasswordUrl = await I.extractUrlFromNotifyEmail(citizenEmail);
+    const resetPasswordUrl = await I.extractUrlFromNotifyEmail(citizenUserPasswordResetEmail);
     I.amOnPage(resetPasswordUrl);
     I.waitForText('Create a new password');
     I.seeTitleEquals('Reset Password - HMCTS Access');
@@ -78,4 +87,4 @@ Scenario('@crossbrowser Idam Web public cross browser tests', async ({ I }) => {
     I.click('Continue');
     I.waitForText('Your password has been changed');
     I.see('You can now sign in with your new password.');
-});
+}).retry(TestData.SCENARIO_RETRY_LIMIT);
