af7bba9ed48bfd87547a34aafcfc59f1a709a419
shravan mechineni <shravanmechineni5@gmail.com>
2021-08-02 16:31:22 +0100

    add mfa org tests

--- src/test/js/mfa_org_e2e_test.js
+++ src/test/js/mfa_org_e2e_test.js
@@ -1,6 +1,5 @@
 const randomData = require('./shared/random_data');
 const TestData = require('./config/test_data');
-const assert = require('assert');
 const jwt_decode = require('jwt-decode');
 const deepEqualInAnyOrder = require('deep-equal-in-any-order');
 const chai = require('chai');
@@ -20,10 +19,11 @@ const orgMFAActiveCompanyNumber = randomData.getRandomAlphabeticString(7);
 let token;
 let serviceToken;
 let prdAuthToken;
-let orgMFADisabledId;
-let orgMFAActiveId;
+let isRefDataEnabled;
 let professionalUserMFASkipEmail;
 let professionalUserMFASkipFirstName;
+let professionalUserMFADisabledEmail;
+let professionalUserMFADisabledFirstName;
 let professionalUserMFARequiredEmail;
 let professionalUserMFARequiredFirstName;
 let mfaTurnedOnService;
@@ -33,62 +33,70 @@ let professionalRoleName;
 BeforeSuite(async ({ I }) => {
 
     token = await I.getAuthToken();
-    mfaTurnedOnServiceName = randomData.getRandomServiceName(testSuiteId);
+    const mfaTurnedOnServiceName = randomData.getRandomServiceName(testSuiteId);
     mfaTurnedOnService = await I.createNewServiceWithRoles(mfaTurnedOnServiceName, serviceClientSecret,  [], '', token, scope);
 
     mfaApplicationPolicyName = `MfaByApplicationPolicy-${mfaTurnedOnService.oauth2ClientId}`;
     await I.createPolicyForApplicationMfaTest(mfaApplicationPolicyName, mfaTurnedOnService.activationRedirectUrl, token);
 
     professionalRoleName = randomData.getRandomRoleName(testSuiteId);
-    professionalRole = await I.createRole(professionalRoleName, 'professional description', '', token);
+    await I.createRole(professionalRoleName, 'professional description', '', token);
 
-    isRefDataEnabled = await I.refDataEnabled()
+    isRefDataEnabled = await I.refDataEnabled();
     if (isRefDataEnabled) {
         professionalUserMFASkipEmail = "freg-test-user-idamskipmfa" + testSuiteId + "@prdfunctestuser.com";
+        professionalUserMFADisabledEmail = "freg-test-user-idammfadisabled" + testSuiteId + "@prdfunctestuser.com";
         professionalUserMFARequiredEmail = "freg-test-user-idamrequiremfa" + testSuiteId + "@prdfunctestuser.com";
     } else {
         // Note that @mfaskip.local works with the testing-support mock of the ref data org/mfa endpoint
         professionalUserMFASkipEmail = "professional" + testSuiteId + "@mfaskip.local";
+        professionalUserMFADisabledEmail = randomData.getRandomEmailAddress();
         professionalUserMFARequiredEmail = randomData.getRandomEmailAddress();
     }
 
     professionalUserMFASkipFirstName = randomData.getRandomUserName(testSuiteId);
-    professionalUserMFASkip = await I.createUserWithRoles(professionalUserMFASkipEmail, userPassword, professionalUserMFASkipFirstName, [professionalRoleName]);
+    await I.createUserWithRoles(professionalUserMFASkipEmail, userPassword, professionalUserMFASkipFirstName, [professionalRoleName]);
 
-    professionalUserMFARequiredFirstName = randomData.getRandomUserName(testSuiteId)
-    professionalUserMFARequired = await I.createUserWithRoles(professionalUserMFARequiredEmail, userPassword, professionalUserMFARequiredFirstName, [professionalRoleName]);
+    professionalUserMFADisabledFirstName = randomData.getRandomUserName(testSuiteId);
+    await I.createUserWithRoles(professionalUserMFADisabledEmail, userPassword, professionalUserMFADisabledFirstName, [professionalRoleName, 'idam-mfa-disabled']);
+
+    professionalUserMFARequiredFirstName = randomData.getRandomUserName(testSuiteId);
+    await I.createUserWithRoles(professionalUserMFARequiredEmail, userPassword, professionalUserMFARequiredFirstName, [professionalRoleName]);
 
     if (isRefDataEnabled) {
 
         // create ref data admin user
-        prdAdminUserEmail = randomData.getRandomEmailAddress();
-        prdAdminUser = await I.createUserWithRoles(prdAdminUserEmail, userPassword, randomData.getRandomUserName(testSuiteId), ['prd-admin']);
+        const prdAdminUserEmail = randomData.getRandomEmailAddress();
+        await I.createUserWithRoles(prdAdminUserEmail, userPassword, randomData.getRandomUserName(testSuiteId), ['prd-admin']);
         prdAuthToken = await I.getAccessTokenPasswordGrant(prdAdminUserEmail, userPassword, mfaTurnedOnService.label, mfaTurnedOnService.activationRedirectUrl, serviceClientSecret, scope);
 
         serviceToken = await I.getServiceAuthToken();
         
         // create organisation with MFA disabled
         let orgMFADisabled = await I.getTestOrganisation(orgMFADisabledCompanyNumber);
-        orgMFADisabledDetails = await I.createOrganisation(orgMFADisabled, serviceToken, prdAuthToken)
-        orgMFADisabledId = orgMFADisabledDetails.organisationIdentifier;
+        let orgMFADisabledDetails = await I.createOrganisation(orgMFADisabled, serviceToken, prdAuthToken);
+        const orgMFADisabledId = orgMFADisabledDetails.organisationIdentifier;
         await I.updateOrganisation(orgMFADisabledId, orgMFADisabled, serviceToken, prdAuthToken);
         await I.updateOrganisationMFA(orgMFADisabledId, "NONE", serviceToken, prdAuthToken);
 
         // Add professional user to organisation with MFA disabled
-        userAddDetails = await I.addUserToOrganisation(orgMFADisabledId, professionalUserMFASkipEmail, "caseworker", serviceToken, prdAuthToken);
+        let userAddDetails = await I.addUserToOrganisation(orgMFADisabledId, professionalUserMFASkipEmail, "caseworker", serviceToken, prdAuthToken);
         console.log("Added user to mfa disabled org: " + JSON.stringify(userAddDetails));
 
         // create organisation with MFA active
         let orgMFAActive = await I.getTestOrganisation(orgMFAActiveCompanyNumber);
-        orgMFAActiveDetails = await I.createOrganisation(orgMFAActive, serviceToken, prdAuthToken)
-        orgMFAActiveId = orgMFAActiveDetails.organisationIdentifier;
+        let orgMFAActiveDetails = await I.createOrganisation(orgMFAActive, serviceToken, prdAuthToken);
+        const orgMFAActiveId = orgMFAActiveDetails.organisationIdentifier;
         await I.updateOrganisation(orgMFAActiveId, orgMFAActive, serviceToken, prdAuthToken);
         await I.updateOrganisationMFA(orgMFAActiveId, "EMAIL", serviceToken, prdAuthToken);
 
-        // Add professional user to organisation with MFA disabled
+        // Add professional user to organisation with MFA enabled
         userAddDetails = await I.addUserToOrganisation(orgMFAActiveId, professionalUserMFARequiredEmail, "caseworker", serviceToken, prdAuthToken);
         console.log("Added user to mfa active org: " + JSON.stringify(userAddDetails));
 
+        // Add idam-mfa-disabled role professional user to organisation with MFA enabled
+        userAddDetails = await I.addUserToOrganisation(orgMFAActiveId, professionalUserMFADisabledEmail, "caseworker", serviceToken, prdAuthToken);
+        console.log("Added mfa disabled user to mfa active org: " + JSON.stringify(userAddDetails));
     } else {
         console.log("ref data integration is disabled");
     }
@@ -122,22 +130,22 @@ Scenario('@functional @mfaOrgLogin I am able to login without MFA as a member of
 
     let jwtDecode = await jwt_decode(accessToken);
 
-    assert.equal("access_token", jwtDecode.tokenName);
-    assert.equal(nonce, jwtDecode.nonce);
-    assert.equal(0, jwtDecode.auth_level);
+    expect(jwtDecode.tokenName).to.equal("access_token");
+    expect(jwtDecode.nonce).to.equal(nonce);
+    expect(jwtDecode.auth_level).to.equal(0);
 
     //Webpublic OIDC userinfo
     const oidcUserInfo = await I.retry({retries: 3, minTimeout: 10000}).getWebpublicOidcUserInfo(accessToken);
-    assert.equal(professionalUserMFASkipEmail.toUpperCase(), oidcUserInfo.sub.toUpperCase());
-    assert.equal(professionalRoleName, oidcUserInfo.roles[0])
-    assert.equal(professionalUserMFASkipFirstName + ' User', oidcUserInfo.name);
-    assert.equal(professionalUserMFASkipFirstName, oidcUserInfo.given_name);
-    assert.equal('User', oidcUserInfo.family_name);
+    expect(oidcUserInfo.sub.toUpperCase()).to.equal(professionalUserMFASkipEmail.toUpperCase());
+    expect(oidcUserInfo.name).to.equal(professionalUserMFASkipFirstName + ' User');
+    expect(oidcUserInfo.given_name).to.equal(professionalUserMFASkipFirstName);
+    expect(oidcUserInfo.family_name).to.equal('User');
 
-    if (orgMFADisabledId) {
+    if (isRefDataEnabled) {
         expect(oidcUserInfo.roles).to.deep.equalInAnyOrder([professionalRoleName, 'caseworker']);
     } else {
-        assert.equal(professionalRoleName, oidcUserInfo.roles[0])
+        // assert.equal(professionalRoleName, oidcUserInfo.roles[0])
+        expect(oidcUserInfo.roles[0]).to.equal(professionalRoleName);
     }
 
     I.resetRequestInterception();
@@ -171,21 +179,62 @@ Scenario('@functional @mfaOrgLogin I am able to login with MFA as a member of an
 
     let jwtDecode = await jwt_decode(accessToken);
 
-    assert.equal("access_token", jwtDecode.tokenName);
-    assert.equal(nonce, jwtDecode.nonce);
-    assert.equal(1, jwtDecode.auth_level);
+    expect(jwtDecode.tokenName).to.equal("access_token");
+    expect(jwtDecode.nonce).to.equal(nonce);
+    expect(jwtDecode.auth_level).to.equal(1);
 
     //Webpublic OIDC userinfo
     const oidcUserInfo = await I.retry({retries: 3, minTimeout: 10000}).getWebpublicOidcUserInfo(accessToken);
-    assert.equal(professionalUserMFARequiredEmail.toUpperCase(), oidcUserInfo.sub.toUpperCase());
-    assert.equal(professionalUserMFARequiredFirstName + ' User', oidcUserInfo.name);
-    assert.equal(professionalUserMFARequiredFirstName, oidcUserInfo.given_name);
-    assert.equal('User', oidcUserInfo.family_name);
+    expect(oidcUserInfo.sub.toUpperCase()).to.equal(professionalUserMFARequiredEmail.toUpperCase());
+    expect(oidcUserInfo.name).to.equal(professionalUserMFARequiredFirstName + ' User');
+    expect(oidcUserInfo.given_name).to.equal(professionalUserMFARequiredFirstName);
+    expect(oidcUserInfo.family_name).to.equal('User');
 
-    if (orgMFAActiveId) {
+    if (isRefDataEnabled) {
         expect(oidcUserInfo.roles).to.deep.equalInAnyOrder([professionalRoleName, 'caseworker']);
     } else {
-        assert.equal(professionalRoleName, oidcUserInfo.roles[0])
+        expect(oidcUserInfo.roles[0]).to.equal(professionalRoleName);
+    }
+
+    I.resetRequestInterception();
+
+}).retry(TestData.SCENARIO_RETRY_LIMIT);
+
+Scenario('@functional @mfaOrgLogin I am able to login without MFA as an idam-mfa-disabled role user but a member of an organisation that has MFA active', async ({ I }) => {
+
+    const loginUrl = `${TestData.WEB_PUBLIC_URL}/login?redirect_uri=${mfaTurnedOnService.activationRedirectUrl}&client_id=${mfaTurnedOnService.oauth2ClientId}&state=${state}&nonce=${nonce}&response_type=code&scope=${scope}&prompt=`;
+
+    I.amOnPage(loginUrl);
+    I.waitForText('Sign in');
+    I.fillField('#username', professionalUserMFADisabledEmail);
+    I.fillField('#password', userPassword);
+    I.interceptRequestsAfterSignin();
+    I.click('Sign in');
+    I.waitForText(mfaTurnedOnService.activationRedirectUrl.toLowerCase());
+    I.see('code=');
+    I.dontSee('error=');
+
+    const pageSource = await I.grabSource();
+    const code = pageSource.match(/\?code=([^&]*)(.*)/)[1];
+    const accessToken = await I.getAccessToken(code, mfaTurnedOnService.oauth2ClientId, mfaTurnedOnService.activationRedirectUrl, serviceClientSecret);
+
+    let jwtDecode = await jwt_decode(accessToken);
+
+    expect(jwtDecode.tokenName).to.equal("access_token");
+    expect(jwtDecode.nonce).to.equal(nonce);
+    expect(jwtDecode.auth_level).to.equal(0);
+
+    //Webpublic OIDC userinfo
+    const oidcUserInfo = await I.retry({retries: 3, minTimeout: 10000}).getWebpublicOidcUserInfo(accessToken);
+    expect(oidcUserInfo.sub.toUpperCase()).to.equal(professionalUserMFADisabledEmail.toUpperCase());
+    expect(oidcUserInfo.name).to.equal(professionalUserMFADisabledFirstName + ' User');
+    expect(oidcUserInfo.given_name).to.equal(professionalUserMFADisabledFirstName);
+    expect(oidcUserInfo.family_name).to.equal('User');
+
+    if (isRefDataEnabled) {
+        expect(oidcUserInfo.roles).to.deep.equalInAnyOrder([professionalRoleName, 'idam-mfa-disabled', 'caseworker']);
+    } else {
+        expect(oidcUserInfo.roles[0]).to.equal(professionalRoleName, 'idam-mfa-disabled');
     }
 
     I.resetRequestInterception();
