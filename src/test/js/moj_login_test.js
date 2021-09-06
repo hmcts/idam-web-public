839004ac875fe9561e096576eb6632db4081ac99
Nikola Naydenov <nikolanaydenov@nikolas-mbp.home>
2021-09-06 01:03:52 +0100

    Add MoJ Functional test

---
+++ src/test/js/moj_login_test.js
@@ -0,0 +1,98 @@
+const chai = require('chai');
+const {expect} = chai;
+const TestData = require('./config/test_data');
+const randomData = require('./shared/random_data');
+
+Feature('moj login tests');
+
+let serviceNames = [];
+
+const testSuitePrefix = randomData.getRandomAlphabeticString();
+const serviceName = randomData.getRandomServiceName(testSuitePrefix);
+const serviceClientSecret = randomData.getRandomClientSecret();
+
+BeforeSuite(async ({ I }) => {
+    const token = await I.getAuthToken();
+    await I.createService(serviceName, serviceClientSecret, '', token, 'openid profile roles', [TestData.MOJ_SSO_PROVIDER_KEY]);
+    serviceNames.push(serviceName);
+});
+
+AfterSuite(async ({ I }) => {
+    I.deleteUser(TestData.MOJ_TEST_USER_USERNAME);
+    return await I.deleteAllTestData(randomData.TEST_BASE_PREFIX + testSuitePrefix);
+});
+
+Scenario('@functional @moj As an Justice.gov.uk user, I can login into idam through OIDC', async ({ I }) => {
+    I.amOnPage(TestData.WEB_PUBLIC_URL + `/o/authorize?login_hint=${TestData.MOJ_SSO_PROVIDER_KEY}&client_id=${serviceName}&redirect_uri=${TestData.SERVICE_REDIRECT_URI}&response_type=code&scope=openid profile roles`);
+    I.waitInUrl('/login/oauth2/code/oidc');
+    I.waitForText('Sign in');
+    I.fillField('loginfmt', TestData.MOJ_TEST_USER_USERNAME);
+    I.click('Next');
+    I.waitForText('Enter password');
+    I.fillField('passwd', TestData.MOJ_TEST_USER_PASSWORD);
+    I.click('Sign in');
+    I.waitForText('Stay signed in?');
+
+    if (TestData.WEB_PUBLIC_URL.includes("-pr-") || TestData.WEB_PUBLIC_URL.includes("staging")) {
+        I.click('No');
+        // expected to be not redirected with the code for pr and staging urls as they're not registered with AAD.
+        I.waitInUrl("/kmsi");
+        I.see("The reply URL specified in the request does not match the reply URLs configured for the application");
+    } else {
+        I.interceptRequestsAfterSignin();
+        I.click('No');
+        I.waitForText(TestData.SERVICE_REDIRECT_URI);
+        I.see('code=');
+        I.dontSee('error=');
+
+        const pageSource = await I.grabSource();
+        const code = pageSource.match(/\?code=([^&]*)(.*)/)[1];
+        const accessToken = await I.getAccessToken(code, serviceName, TestData.SERVICE_REDIRECT_URI, serviceClientSecret);
+
+        const userInfo = await I.retry({retries: 3, minTimeout: 10000}).getUserInfo(accessToken);
+        expect(userInfo.active).to.equal(true);
+        expect(userInfo.email).to.equal(TestData.MOJ_TEST_USER_USERNAME);
+        expect(userInfo.id).to.not.equal(null);
+    }
+
+    I.resetRequestInterception();
+}).retry(TestData.SCENARIO_RETRY_LIMIT);
+
+Scenario('@functional @moj As an Justice.gov.uk user, I should be able to login through the Justice.gov.uk login link from idam', async ({ I }) => {
+    I.amOnPage(TestData.WEB_PUBLIC_URL + `/login?client_id=${serviceName}&redirect_uri=${TestData.SERVICE_REDIRECT_URI}&response_type=code&scope=openid profile roles`);
+    I.waitForText('Sign in');
+    I.waitForText('Log in with your Justice.gov.uk account');
+    I.click('Log in with your Justice.gov.uk account');
+    I.waitInUrl('/oauth2/authorize');
+    I.waitForText('Sign in');
+    I.fillField('loginfmt', TestData.MOJ_TEST_USER_USERNAME);
+    I.click('Next');
+    I.waitForText('Enter password');
+    I.fillField('passwd', TestData.MOJ_TEST_USER_PASSWORD);
+    I.click('Sign in');
+
+    I.waitForText('Stay signed in?');
+    if (TestData.WEB_PUBLIC_URL.includes("-pr-") || TestData.WEB_PUBLIC_URL.includes("staging")) {
+        I.click('No');
+        // expected to be not redirected with the code for pr and staging urls as they're not registered with AAD.
+        I.waitInUrl("/kmsi");
+        I.see("The reply URL specified in the request does not match the reply URLs configured for the application");
+    } else {
+        I.interceptRequestsAfterSignin();
+        I.click('No');
+        I.waitForText(TestData.SERVICE_REDIRECT_URI);
+        I.see('code=');
+        I.dontSee('error=');
+
+        const pageSource = await I.grabSource();
+        const code = pageSource.match(/\?code=([^&]*)(.*)/)[1];
+        const accessToken = await I.getAccessToken(code, serviceName, TestData.SERVICE_REDIRECT_URI, serviceClientSecret);
+
+        const userInfo = await I.retry({retries: 3, minTimeout: 10000}).getUserInfo(accessToken);
+        expect(userInfo.active).to.equal(true);
+        expect(userInfo.email).to.equal(TestData.MOJ_TEST_USER_USERNAME);
+        expect(userInfo.id).to.not.equal(null);
+    }
+
+    I.resetRequestInterception();
+});
