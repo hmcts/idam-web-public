ebadce946636d2041f57f865a615ff1397593623
James Burke (IDAM) <james.burke@hmcts.net>
2021-10-11 16:30:29 +0100

    add lettuce client timeouts and behaviour

--- src/main/java/uk/gov/hmcts/reform/idam/web/config/SessionConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/idam/web/config/SessionConfiguration.java
@@ -1,13 +1,21 @@
 package uk.gov.hmcts.reform.idam.web.config;
 
+import io.lettuce.core.ClientOptions;
+import io.lettuce.core.SocketOptions;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.session.data.redis.config.ConfigureRedisAction;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.springframework.session.web.http.CookieSerializer;
 import org.springframework.session.web.http.DefaultCookieSerializer;
+
+import java.time.Duration;
 
 @Configuration
 @EnableRedisHttpSession
@@ -15,6 +23,18 @@ import org.springframework.session.web.http.DefaultCookieSerializer;
 public class SessionConfiguration {
 
     private static final int FIVE_DAYS_IN_SECONDS = 432000;
+
+    @Value("${spring.redis.host}")
+    private String redisHostName;
+
+    @Value("${spring.redis.port}")
+    private int redisPort;
+
+    private @Value("${spring.redis.custom.command.timeout}")
+    Duration redisCommandTimeout;
+
+    private @Value("${spring.redis.timeout}")
+    Duration socketTimeout;
 
     @Value("${authentication.secureCookie}")
     private Boolean useSecureCookie;
@@ -28,6 +48,28 @@ public class SessionConfiguration {
         serializer.setCookieMaxAge(FIVE_DAYS_IN_SECONDS);
         serializer.setUseSecureCookie(useSecureCookie);
         return serializer;
+    }
+
+    @Bean
+    public LettuceConnectionFactory connectionFactory() {
+        final SocketOptions socketOptions = SocketOptions.builder().connectTimeout(socketTimeout).build();
+
+        final ClientOptions clientOptions = ClientOptions.builder()
+            .socketOptions(socketOptions)
+            .cancelCommandsOnReconnectFailure(true)
+            .disconnectedBehavior(ClientOptions.DisconnectedBehavior.REJECT_COMMANDS)
+            .build();
+
+        LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
+            .commandTimeout(redisCommandTimeout)
+            .clientOptions(clientOptions)
+            .build();
+        RedisStandaloneConfiguration serverConfig = new RedisStandaloneConfiguration(redisHostName,
+            redisPort);
+
+        final LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory(serverConfig,
+            clientConfig);
+        return lettuceConnectionFactory;
     }
 
     /*
