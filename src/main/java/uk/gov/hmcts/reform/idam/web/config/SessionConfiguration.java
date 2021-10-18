d9f0571a9e01665c62e1d87e02ab7035fdd40a57
James Burke (IDAM) <james.burke@hmcts.net>
2021-10-18 14:53:51 +0100

    define redis in code

--- src/main/java/uk/gov/hmcts/reform/idam/web/config/SessionConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/idam/web/config/SessionConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.session.data.redis.config.ConfigureRedisAction;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.springframework.session.web.http.CookieSerializer;
@@ -28,6 +29,11 @@ public class SessionConfiguration {
         serializer.setCookieMaxAge(FIVE_DAYS_IN_SECONDS);
         serializer.setUseSecureCookie(useSecureCookie);
         return serializer;
+    }
+
+    @Bean
+    public LettuceConnectionFactory connectionFactory() {
+        return new LettuceConnectionFactory();
     }
 
     /*
---
+++ src/main/java/uk/gov/hmcts/reform/idam/web/config/SessionInitializer.java
@@ -0,0 +1,10 @@
+package uk.gov.hmcts.reform.idam.web.config;
+
+import org.springframework.session.web.context.AbstractHttpSessionApplicationInitializer;
+
+public class SessionInitializer extends AbstractHttpSessionApplicationInitializer {
+
+    public SessionInitializer() {
+        super(SessionConfiguration.class);
+    }
+}
