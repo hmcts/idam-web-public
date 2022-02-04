9d2b07a20266f58e49ededd1163276666a6986f8
Kremi Nenkova <kremi.nenkova@gmail.com>
2022-02-04 12:25:42 +0000

    Invalidate both Idam.SSOSession and Idam.Session cookies.

--- src/main/java/uk/gov/hmcts/reform/idam/web/strategic/EndSessionFilter.java
+++ src/main/java/uk/gov/hmcts/reform/idam/web/strategic/EndSessionFilter.java
@@ -1,10 +1,13 @@
 package uk.gov.hmcts.reform.idam.web.strategic;
 
+import com.google.common.net.HttpHeaders;
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
 import org.springframework.stereotype.Component;
+import uk.gov.hmcts.reform.idam.web.config.properties.ConfigurationProperties;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.POST_TYPE;
@@ -14,6 +17,12 @@ import static org.springframework.cloud.netflix.zuul.filters.support.FilterConst
 public class EndSessionFilter extends ZuulFilter {
 
     private static final String OIDC_END_SESSION_ENDPOINT = "/o/endSession";
+
+    private final String idamSessionCookieName;
+
+    public EndSessionFilter(ConfigurationProperties configurationProperties) {
+        this.idamSessionCookieName = configurationProperties.getStrategic().getSession().getIdamSessionCookie();
+    }
 
     @Override
 	public String filterType() {
@@ -33,12 +42,17 @@ public class EndSessionFilter extends ZuulFilter {
 
     @Override
 	public Object run() {
-		RequestContext context = RequestContext.getCurrentContext();
+        RequestContext context = RequestContext.getCurrentContext();
         HttpServletRequest httpRequest = context.getRequest();
         HttpSession session = httpRequest.getSession(false);
         if (session != null) {
             session.invalidate();
         }
+
+        HttpServletResponse servletResponse = context.getResponse();
+        servletResponse.addHeader(HttpHeaders.SET_COOKIE, idamSessionCookieName
+            + "=; Path=/; Expires=Thu, 01-Jan-1970 00:00:10 GMT; HttpOnly");
+
         return null;
 	}
 
