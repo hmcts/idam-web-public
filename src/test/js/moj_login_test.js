4fe3bf8c873549d212ab9acd7b80875c5dce4f1c
Shravan Mechineni <shravanmechineni5@gmail.com>
2022-02-08 09:39:46 +0000

    Sidm 7004 exclude sso tests (#760)
    * exclude sso tests from ithc and perftest envt
    
    * include ejudiciary tests for ithc
    
    * include moj sso tests for ithc

--- src/test/js/moj_login_test.js
+++ src/test/js/moj_login_test.js
@@ -2,8 +2,13 @@ const chai = require('chai');
 const {expect} = chai;
 const TestData = require('./config/test_data');
 const randomData = require('./shared/random_data');
+let isEnvtPerftest = TestData.WEB_PUBLIC_URL.includes("perftest");
 
-Feature('moj login tests');
+if (isEnvtPerftest){
+    xFeature('moj login tests');
+} else {
+    Feature('moj login tests');
+}
 
 let serviceNames = [];
 
