cb8bb94600a321c3a31ecf5a841543deb1e5ad20
Nikola Naydenov <nikolanaydenov@nikolas-mbp.home>
2022-02-22 01:59:11 +0000

    SIDM-7101 Everythings okay

---
+++ src/main/java/uk/gov/hmcts/reform/idam/web/error/EverythingsOkayErrorController.java
@@ -0,0 +1,30 @@
+package uk.gov.hmcts.reform.idam.web.error;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Controller
+public class EverythingsOkayErrorController implements ErrorController {
+
+    public static final String ERROR_VIEW = "error";
+
+    @RequestMapping("/error")
+    public String error(HttpServletRequest request, HttpServletResponse response) {
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+
+        if (status != null) {
+            int statusCode = Integer.parseInt(status.toString());
+            if(statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+                response.setStatus(200);
+            }
+        }
+
+        return ERROR_VIEW;
+    }
+}
