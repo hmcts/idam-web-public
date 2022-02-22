00984ee8a75a0a89b06b6a639687417e8ab0fc37
Nikola Naydenov <nikolanaydenov@nikolas-mbp.home>
2022-02-22 02:43:29 +0000

    Add extra exception logging

--- src/main/java/uk/gov/hmcts/reform/idam/web/error/EverythingsOkayErrorController.java
+++ src/main/java/uk/gov/hmcts/reform/idam/web/error/EverythingsOkayErrorController.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.idam.web.error;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
@@ -10,6 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @Controller
+@Slf4j
 public class EverythingsOkayErrorController implements ErrorController {
 
     public static final String ERROR_VIEW = "error";
@@ -17,11 +19,15 @@ public class EverythingsOkayErrorController implements ErrorController {
     @RequestMapping("/error")
     public String error(HttpServletRequest request, HttpServletResponse response) {
         Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        Object exception = request.getAttribute(RequestDispatcher.ERROR_EXCEPTION);
 
         if (status != null) {
             int statusCode = Integer.parseInt(status.toString());
             if(statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
                 response.setStatus(200);
+                if (exception != null) {
+                    log.info("Status 500 error redirect.", (Throwable) exception);
+                }
             }
         }
 
