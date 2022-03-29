38c2d5a4bcedb4043f44ca6d0ff9c42379113ef7
Kremi Nenkova <kremi.nenkova@gmail.com>
2022-03-29 11:07:35 +0100

    Tests for automatic SSO redirection are now in their own separate scenario.

--- src/test/js/ejudiciary_login_test.js
+++ src/test/js/ejudiciary_login_test.js
@@ -107,16 +107,26 @@ Scenario('@functional @ejudiciary As an ejudiciary user, I should be able to log
         expect(userInfo.roles).to.eql(['judiciary']);
 
         I.resetRequestInterception();
-
-        //redirection verification
-        I.amOnPage(TestData.WEB_PUBLIC_URL + `/login?client_id=${serviceName}&redirect_uri=${TestData.SERVICE_REDIRECT_URI}&response_type=code&scope=openid profile roles`);
-        I.waitForText('Sign in');
-        I.fillField('#username', TestData.EJUDICIARY_TEST_USER_USERNAME);
-        I.fillField('#password', TestData.EJUDICIARY_TEST_USER_PASSWORD);
-        I.interceptRequestsAfterSignin();
-        I.click('Sign in');
-        I.waitForText(TestData.WEB_PUBLIC_URL + `/oauth2/authorization/oidc`);
-        I.resetRequestInterception();
     }
+
+}).retry(TestData.SCENARIO_RETRY_LIMIT);
+
+Scenario('@functional @ejudiciary As an ejudiciary user, I should be redirected to eJudiciary for login if I enter my username on the login screen', async ({ I }) => {
+    //redirection verification
+    I.amOnPage(TestData.WEB_PUBLIC_URL + `/login?client_id=${serviceName}&redirect_uri=${TestData.SERVICE_REDIRECT_URI}&response_type=code&scope=openid profile roles`);
+    I.waitForText('Sign in');
+    I.fillField('#username', TestData.EJUDICIARY_TEST_USER_USERNAME);
+    I.fillField('#password', TestData.EJUDICIARY_TEST_USER_PASSWORD);
+    I.click('Sign in');
+
+    I.waitForText('Sign in');
+    I.fillField('loginfmt', TestData.EJUDICIARY_TEST_USER_USERNAME);
+    I.click('Next');
+    I.waitForText('Enter password');
+    I.fillField('passwd', TestData.EJUDICIARY_TEST_USER_PASSWORD);
+    I.click('Sign in');
+
+    I.waitForText('Stay signed in?');
+    I.click('No');
 
 }).retry(TestData.SCENARIO_RETRY_LIMIT);
