4ba445737774cbdda7cff14e035fee5eeca52239
kremi <34029797+kremi@users.noreply.github.com>
2022-12-19 16:34:34 +0000

    SIDM-8310: Handle RequestRejectedExceptions (#838)
    * SIDM-8310: Return HTTP 400 instead of HTTP 500 when a request URL looks suspicious.

--- src/main/java/uk/gov/hmcts/reform/idam/web/config/IdamWebMvcConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/idam/web/config/IdamWebMvcConfiguration.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.idam.web.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.tomcat.util.http.Rfc6265CookieProcessor;
 import org.apache.tomcat.util.http.SameSiteCookies;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,7 +8,11 @@ import org.springframework.boot.web.embedded.tomcat.TomcatContextCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.CacheControl;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.security.web.firewall.HttpStatusRequestRejectedHandler;
+import org.springframework.security.web.firewall.RequestRejectedException;
+import org.springframework.security.web.firewall.RequestRejectedHandler;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -17,6 +22,11 @@ import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.mvc.WebContentInterceptor;
 import uk.gov.hmcts.reform.idam.web.config.properties.ConfigurationProperties;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
 @Configuration
 public class IdamWebMvcConfiguration implements WebMvcConfigurer {
 
@@ -75,6 +85,16 @@ public class IdamWebMvcConfiguration implements WebMvcConfigurer {
         return new RequestMethodInterceptor();
     }
 
+    @Bean
+    public RequestRejectedHandler requestRejectedHandler() {
+        return new HttpStatusRequestRejectedHandler(HttpStatus.BAD_REQUEST.value()) {
+            @Override
+            public void handle(HttpServletRequest request, HttpServletResponse response, RequestRejectedException requestRejectedException) throws IOException {
+                log.error("Request rejected due to Spring Security", requestRejectedException);
+                response.sendError(HttpStatus.BAD_REQUEST.value());
+            }
+        };
+    }
     /**
      * return HTML by default when not sure.
      */
