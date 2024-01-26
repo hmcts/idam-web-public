7618b2d2e653b291e4d2f8cf1480a5e8aa6c478e
Kremi Nenkova <kremi.nenkova@gmail.com>
2024-01-26 09:56:18 +0000

    SSO user might have their correct SSO ID when setting them up for federation

--- src/test/js/moj_login_test.js
+++ src/test/js/moj_login_test.js
@@ -122,7 +122,7 @@ Scenario('@functional @moj As an Justice.gov.uk user, I should be able to login 
 
 Scenario('@functional @moj As a Justice.gov.uk user, I should be redirected to MoJ IDAM for login if I enter my username on the login screen', async ({ I }) => {
     await I.deleteUser(TestData.MOJ_TEST_USER_USERNAME);
-    await I.createUserUsingTestingSupportService(accessToken, TestData.MOJ_TEST_USER_USERNAME, userPassword, randomUserFirstName, [mojUserRole.name], "moj", randomData.getRandomString());
+    await I.createUserUsingTestingSupportService(accessToken, TestData.MOJ_TEST_USER_USERNAME, userPassword, randomUserFirstName, [mojUserRole.name], "moj", TestData.MOJ_TEST_USER_SSO_ID);
     //redirection verification
     I.amOnPage(TestData.WEB_PUBLIC_URL + `/login?client_id=${serviceName}&redirect_uri=${TestData.SERVICE_REDIRECT_URI}&response_type=code&scope=openid profile roles`);
     I.waitForText('Sign in');
