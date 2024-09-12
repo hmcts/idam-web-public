9db3c4552e033c0437ec434a32ff8c10a8a0f1cb
James Burke (IDAM) <james.burke@hmcts.net>
2024-09-12 11:59:37 +0100

    retry failed tests

--- codecept.conf.js
+++ codecept.conf.js
@@ -7,7 +7,7 @@ exports.config = {
     fullPageScreenshots: true,
     timeout: 240, // seconds
     bootstrap: false,
-    retry: 1,
+    retry: 0,
     helpers: {
         Puppeteer: {
             // show: true,
