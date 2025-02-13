00f43cae2cc803dd00fde70cdd947e472d552077
Arun <arun.kumar@hmcts.net>
2025-02-13 17:32:56 +0000

    SIDM-9347 fixed now

--- src/test/js/config/supportedBrowsers.js
+++ src/test/js/config/supportedBrowsers.js
@@ -1,28 +1,28 @@
 const LATEST_MAC = 'macOS 13';
 const LATEST_WINDOWS = 'Windows 11';
 const supportedBrowsers = {
-  microsoft: {
-    edge: {
-      browserName: 'MicrosoftEdge',
-      platformName: LATEST_WINDOWS,
-      browserVersion: 'latest',
-      'sauce:options': {
-        name: 'IDAM: Win_Edge_latest'
-      }
-    }
-  },
-  safari: {
-    safari_mac_latest: {
-      browserName: 'safari',
-      platformName: 'macOS 10.14',
-      browserVersion: 'latest',
-      'sauce:options': {
-        name: 'IDAM: Mac_Safari_latest',
-        seleniumVersion: '3.141.59',
-        screenResolution: '1400x1050'
-      }
-    }
-  },
+    microsoft: {
+        edge: {
+            browserName: 'MicrosoftEdge',
+            platformName: LATEST_WINDOWS,
+            browserVersion: 'latest',
+            'sauce:options': {
+                name: 'IDAM: Win_Edge_latest'
+            }
+        }
+    },
+    safari: {
+        safari_mac_latest: {
+            browserName: 'safari',
+            platformName: 'macOS 10.14',
+            browserVersion: 'latest',
+            'sauce:options': {
+                name: 'IDAM: Mac_Safari_latest',
+                seleniumVersion: '3.141.59',
+                screenResolution: '1400x1050'
+            }
+        }
+    },
     chrome: {
         chrome_win_latest: {
             browserName: 'chrome',
@@ -44,24 +44,24 @@ const supportedBrowsers = {
         }
 
     },
-  firefox: {
-    firefox_win_latest: {
-      browserName: 'firefox',
-      platformName: LATEST_WINDOWS,
-      browserVersion: 'latest',
-      'sauce:options': {
-        name: 'IDAM: Win_Firefox_latest'
-      }
-    },
-    firefox_mac_latest: {
-      browserName: 'firefox',
-      platformName: LATEST_MAC,
-      browserVersion: 'latest',
-      'sauce:options': {
-        name: 'IDAM: Mac_Firefox_latest'
-      }
+    firefox: {
+        firefox_win_latest: {
+            browserName: 'firefox',
+            platformName: LATEST_WINDOWS,
+            browserVersion: 'latest',
+            'sauce:options': {
+                name: 'IDAM: Win_Firefox_latest'
+            }
+        },
+        firefox_mac_latest: {
+            browserName: 'firefox',
+            platformName: LATEST_MAC,
+            browserVersion: 'latest',
+            'sauce:options': {
+                name: 'IDAM: Mac_Firefox_latest'
+            }
+        }
     }
-  }
 };
 
 module.exports = supportedBrowsers;
