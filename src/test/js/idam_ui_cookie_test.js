4ddafdf05c9ce9bb25f247d54475a54781841341
James Burke <james.burke@hmcts.net>
2025-12-02 11:50:39 +0000

    Release 9.22.0 RC1 (#1587)
    * Update dependency org.apache.tomcat.embed:tomcat-embed-core to v9.0.112 (#1576)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    
    * Update dependency org.apache.tomcat.embed:tomcat-embed-el to v9.0.112 (#1577)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    
    * Update dependency org.apache.tomcat.embed:tomcat-embed-jasper to v9.0.112 (#1578)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    
    * Update dependency com.squareup.okio:okio to v3.16.4 (#1575)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    
    * Update dependency org.apache.tomcat.embed:tomcat-embed-websocket to v11.0.14 (#1579)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    
    * Update dependency commons-io:commons-io to v2.21.0 (#1582)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    
    * Update plugin git-properties to v2.5.4 (#1580)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    
    * SIDM-10124: idam.ui cookie (#1584)
    
    * Update dependency org.apache.commons:commons-lang3 to v3.20.0 (#1586)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    
    * Set HttpOnly flag for idam.ui cookie (#1589)
    
    * Set HttpOnly flag for idam.ui cookie
    * Update IdamUiCookieFilterTest.java
    
    ---------
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    Co-authored-by: kremi <34029797+kremi@users.noreply.github.com>

---
+++ src/test/js/idam_ui_cookie_test.js
@@ -0,0 +1,20 @@
+const assert = require('assert');
+const TestData = require('./config/test_data');
+
+const IDAM_UI_COOKIE = 'Idam.UI';
+const serviceName = 'idam-functional-test-service';
+const serviceRedirect = 'https://idam-functional-test-service';
+const selfRegUrl = `${TestData.WEB_PUBLIC_URL}/users/selfRegister?redirect_uri=${serviceRedirect}&client_id=${serviceName}`;
+const loginUrl = `${TestData.WEB_PUBLIC_URL}/login?redirect_uri=${serviceRedirect}&client_id=${serviceName}`;
+const cookiesUrl = `${TestData.WEB_PUBLIC_URL}/cookies`;
+const assetUrl = `${TestData.WEB_PUBLIC_URL}/assets/javascripts/govuk-template.js`;
+
+Scenario('@functional @idamUiCookie', async ({ I }) => {
+    const urls = [selfRegUrl, loginUrl, cookiesUrl, assetUrl];
+    for (const url of urls) {
+        I.amOnPage(url);
+        const cookie = await I.grabCookie(IDAM_UI_COOKIE);
+        assert(cookie !== null);
+        assert.strictEqual(cookie.value, 'classic');
+    }
+});
